75e8652c3b1f99f5f88f25d18cf2b737c95c149b
danielriedmueller <daniel.riedmuller@gmail.com>
2020-08-31 09:57:11 +0200

    Arbeitspaket #2

--- helper/product_attributes_helper.php
+++ helper/product_attributes_helper.php
@@ -22,31 +22,31 @@ function extractOtherAttributes(array $productAttributes): array
     return $productAttributes;
 }
 
-function isProductOfTheMonth(WC_Product_Variable $product): bool
+function isProductOfTheMonth(WC_Product $product): bool
 {
     $value = strtolower($product->get_attribute( PRODUCT_OF_MONTH_ATTRIBUTE_SLUG));
 
     return $value === "ja" ? true : false;
 }
 
-function hasBiosigil(WC_Product_Variable $product): bool
+function hasBiosigil(WC_Product $product): bool
 {
     $value = strtolower($product->get_attribute( BIOSIGIL_SLUG));
 
     return $value === "ja" ? true : false;
 }
 
-function getProductOfTheMonthClass(WC_Product_Variable $product): string
+function getProductOfTheMonthClass(WC_Product $product): string
 {
     return isProductOfTheMonth($product) ? "product-of-month" : "";
 }
 
-function getBiosigilClass(WC_Product_Variable $product): string
+function getBiosigilClass(WC_Product $product): string
 {
     return hasBiosigil($product) ? "biosigil" : "";
 }
 
-function getOriginCountry(WC_Product_Variable $product): string
+function getOriginCountry(WC_Product $product): string
 {
     $attributeArr = getAttributeArray($product, ORIGIN_COUNTRY_ATTRIBUTE_SLUG);
 
@@ -60,7 +60,7 @@ function getOriginCountry(WC_Product_Variable $product): string
 /**
  * Returns a single product attribute as array.
  */
-function getAttributeArray(WC_Product_Variable $product, string $attribute): array
+function getAttributeArray(WC_Product $product, string $attribute): array
 {
     $attributes = $product->get_attributes();
     $attribute  = sanitize_title( $attribute );
