075233b90f4094b9a973e305ed3714cb8c40618c
danielriedmueller <daniel.riedmuller@gmail.com>
2021-01-21 10:27:04 +0100

    Add ticket category to created ticket products and set product_type to simple instead of virtual

--- actions/woocommerce.php
+++ actions/woocommerce.php
@@ -15,9 +15,14 @@ remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
 
 /**
  * Add ticket category to created ticket products
+ * and set product_type to simple instead of virtual
  */
 add_action('event_tickets_after_save_ticket', function ($event_id, $ticket, $raw_data, $classname) {
     if (!empty($ticket) && isset($ticket->ID)) {
-        wp_add_object_terms($ticket->ID, TICKET_CATEGORY_SLUG, 'product_cat');
+        $cat = get_term_by( 'slug', TICKET_CATEGORY_SLUG, 'product_cat' );
+        $product = wc_get_product( $ticket->ID );
+        $product->set_category_ids([$cat->term_id]);
+        $product->set_virtual( false );
+        $product->save();
     }
 }, 10, 4);
