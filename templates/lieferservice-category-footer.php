179c99657c88537d33d8f69db51ea1f0048401ca
danielriedmueller <daniel.riedmuller@gmail.com>
2021-01-28 19:14:48 +0100

    lieferservice redesign

--- templates/lieferservice-category-footer.php
+++ templates/lieferservice-category-footer.php
@@ -1,8 +1,11 @@
 <?php ?>
 <div class="rehorik-lieferservice-category-footer">
-    <div>
-        <div>
-            <div class="category-gap-filler"></div>
+    <div class="order-instructions">
+        <h3>Bestellt, verpackt & auf dem Weg zu dir!</h3>
+        <div>Bestellen Sie bis jeweils MO. 12 Uhr und MI. 12 Uhr per Telefon: <a href="tel:0941/59975895">0941/59975895</a> oder per Email: <a href="mailto:lieferservice@rehorik.de">lieferservice@rehorik.de</a>
+            oder jederzeit im Onlineshop. Die Auslieferung erfolgt am Folgetag, jeweils DI. und DO. ab 13 Uhr – ausgeschlossen davon sind Feiertage.
+            Die Bestellliste zum Download, finden Sie <a href="<?php echo get_stylesheet_directory_uri() ?>/assets/document/Flyer Lieferservice Website DIN A4.pdf">hier</a>.
+            Versandkosten 5,80 €, kostenloser Versand ab 49 €.
         </div>
     </div>
 </div>
--- templates/lieferservice-category-header.php
+++ templates/lieferservice-category-header.php
@@ -1,51 +1,38 @@
 <?php ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
 <div class="rehorik-lieferservice-category-header">
-    <div class="quality-text">
-        <h4>Qualität für Zuhause</h4>
-        <div>Seit einiger Zeit bieten wir für Sie einen Lieferdienst an.</div>
-        <div><b>DOCH WAS STECKT DAHINTER?</b></div>
-        <div class="quality-text-content">
-            <div>
-                Die Grundidee unseres Lieferservices ist es, Ihnen ein regionales Netzwerk mit nachhaltigen Produkten
-                aus unserer Region zu bieten. In Zusammenarbeit mit der Metzgerei Stierstorfer, der Bäckerei Freisleben
-                und dem Gemüsehof Köstlmeier ist es uns ein großes Anliegen, Ihnen die Regensburger Genusswelten
-                näherzubringen und unser fein ausgewähltes Produktsortiment mit Ihnen zu teilen.
-            </div>
-            <div>
-                Die Kooperation mit dem Fahrradkurier "Feine Fracht" rundet für uns den Gedanken eines nachhaltigen
-                Lieferservices perfekt ab.<br>
-                Gerne beraten wir Sie auch per Telefon: <a href="tel:0941/59975895">0941/59975895</a>
-                oder per Email: <a href="mailto:lieferservice@rehorik.de">lieferservice@rehorik.de</a> und stellen gemeinsam mit Ihnen ein individuelles Paket
-                zusammen.
+    <div class="quality-text-outer">
+        <div class="quality-text">
+            <h3>Qualität für Zuhause</h3>
+            <div class="quality-text-content">
+                <div>
+                    Seit einiger Zeit bieten wir für Sie einen Lieferdienst an. Doch was steckt dahinter?
+                    Die Grundidee unseres Lieferservices ist es, Ihnen ein regionales Netzwerk mit nachhaltigen Produkten
+                    aus unserer Region zu bieten. In Zusammenarbeit mit der Metzgerei Stierstorfer, der Bäckerei Freisleben
+                    und dem Gemüsehof Köstlmeier ist es uns ein großes Anliegen, Ihnen die Regensburger Genusswelten näherzubringen
+                </div>
+                <div>
+                    und unser fein ausgewähltes Produktsortiment mit Ihnen zu teilen.
+                    Die Kooperation mit dem Fahrradkurier "Feine Fracht" rundet für uns den Gedanken eines nachhaltigen
+                    Lieferservices perfekt ab.
+                    Gerne beraten wir Sie auch per Telefon: <a href="tel:0941/59975895">0941/59975895</a>
+                    oder per Email: <a href="mailto:lieferservice@rehorik.de">lieferservice@rehorik.de</a> und stellen gemeinsam mit Ihnen ein individuelles Paket
+                    zusammen.
+                </div>
             </div>
         </div>
     </div>
     <ul id="slider">
         <li>
-            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/lieferservice/header_lieferservice_1920x600px_v1.jpg">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/lieferservice/lieferservice-header-600px-v1.jpg">
         </li>
         <li>
-            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/lieferservice/header_lieferservice_1920x600px_v2.jpg">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/lieferservice/lieferservice-header-600px-v2.jpg">
         </li>
     </ul>
     <div id="tns-controls-container">
         <button></button>
         <button></button>
-    </div>
-    <div class="order-instructions-outer">
-        <div class="order-instructions">
-            <div>
-                <h3>BESTELLT, VERPACKT <br>& AUF DEM WEG ZU DIR!</h3>
-                <div>Bestellen Sie bis jeweils Montag 12 Uhr und Mittwoch 12 Uhr per Telefon: <a href="tel:0941/59975895">0941/59975895</a> oder per Email: <a href="mailto:lieferservice@rehorik.de">lieferservice@rehorik.de</a>
-                    oder jederzeit im Onlineshop.<br>
-                    Die Auslieferung erfolgt am Folgetag, jeweils dienstags und
-                    donnerstags ab 13 Uhr – ausgeschlossen davon sind Feiertage.
-                    Unsere Bestellliste zum Download, finden Sie <a href="<?php echo get_stylesheet_directory_uri() ?>/assets/document/Flyer Lieferservice Website DIN A4.pdf">hier</a>.
-                    Die Versandkosten betragen 5,8 €, kostenloser Versand ab 49 €.
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 
