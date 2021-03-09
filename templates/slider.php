4fc3a70a2822740eda43c6d251e9b8bcd102fe0c
danielriedmueller <daniel.riedmuller@gmail.com>
2021-03-09 15:33:50 +0100

    design startseite

--- templates/slider.php
+++ templates/slider.php
@@ -4,13 +4,6 @@
     <?php foreach($args['images'] as $image): ?>
         <li>
             <img src="<?= get_stylesheet_directory_uri() . $image ?>" />
-            <div class="slider-claim">
-                <h2>Kaffeerösterei seit 1928</h2>
-                <div>
-                    <a class="button" href="<?= get_term_link( get_term_by('slug', COFFEE_CATEGORY_SLUG, 'product_cat'), 'product_cat' ); ?>">Zum Kaffee</a>
-                    <a class="button" href="#">Unsere Kugelröster</a>
-                </div>
-            </div>
         </li>
     <?php endforeach; ?>
 </ul>
