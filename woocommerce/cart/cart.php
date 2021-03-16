a8cde82caa73519e128a9601a6ccfc10f490b727
danielriedmueller <daniel.riedmuller@gmail.com>
2021-03-16 15:26:48 +0100

    cuoupon bugfix

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -129,15 +129,25 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 			<?php do_action( 'woocommerce_cart_contents' ); ?>
 
-            <tr class="rehorik-coupon-row-small">
-                <td colspan="4">
-                    <?php require('cart-coupon.php'); ?>
-                </td>
-            </tr>
-            <tr class="rehorik-coupon-row">
-                <td></td>
-                <td colspan="3">
-                    <?php require('cart-coupon.php'); ?>
+            <tr>
+                <td colspan="6" class="actions">
+                    <div class="rehorik-coupon-container">
+                        <?php if ( wc_coupons_enabled() ) { ?>
+                            <div class="rehorik-coupon">
+                                <label>
+                                    <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="Gutscheincode eingeben" />
+                                    <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>">Einlösen</button>
+                                </label>
+                                <?php do_action( 'woocommerce_cart_coupon' ); ?>
+                            </div>
+                        <?php } ?>
+
+                        <div class="rehorik-update-cart">
+                            <button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
+                            <?php do_action( 'woocommerce_cart_actions' ); ?>
+                            <?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
+                        </div>
+                    </div>
                 </td>
             </tr>
 
