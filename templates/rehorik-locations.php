aab69cbd62a43316630c581d9006cce7ff9c59a1
danielriedmueller <daniel.riedmuller@gmail.com>
2021-03-24 12:04:10 +0100

    typo

--- templates/rehorik-locations.php
+++ templates/rehorik-locations.php
@@ -6,8 +6,8 @@
             <div>
                 <img alt="Standort Stammhaus" src="<?= get_stylesheet_directory_uri() . '/assets/img/standorte/footer-standorte-320x213px-stammhaus.png' ?>">
                 <h2>Stammhaus</h2>
-                <p>Mit <a href="<?= get_page_link(ROESTEREI_PAGE_ID) ?>">Kaffeerösteri</a> | <a href="<?= get_page_link(CAFE_190_PAGE_ID) ?>">Cafe 190°</a> | <a href="<?= get_page_link(STAMMHAUS_PAGE_ID) ?>#weinkeller">Weinkeller</a> | <a href="<?= get_page_link(STAMMHAUS_PAGE_ID) ?>#kaeseladen">Käseladen</a></p>
-                <p>Am Brixener Hof 7</p>
+                <p>Mit <a href="<?= get_page_link(ROESTEREI_PAGE_ID) ?>">Kaffeerösterei</a> | <a href="<?= get_page_link(CAFE_190_PAGE_ID) ?>">Cafe 190°</a> | <a href="<?= get_page_link(STAMMHAUS_PAGE_ID) ?>#weinkeller">Weinkeller</a> | <a href="<?= get_page_link(STAMMHAUS_PAGE_ID) ?>#kaeseladen">Käseladen</a></p>
+                <p>Am Brixener Hof 6</p>
                 <p>93047 Regensburg</p>
             </div>
             <div>
@@ -21,7 +21,7 @@
                 <img alt="Standort Barista 120" src="<?= get_stylesheet_directory_uri() . '/assets/img/standorte/footer-standorte-320x213px-120.png' ?>">
                 <h2>Barista Store 120°</h2>
                 <p>Maschinenberatung | Kaffeeseminare</p>
-                <p>Am Brixener Hof 7</p>
+                <p>Am Brixener Hof 6</p>
                 <p>93047 Regensburg</p>
             </div>
             <div>
