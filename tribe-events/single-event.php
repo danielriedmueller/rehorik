3c322dff93b71340615a75ab9ba9b7759c03229e
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2022-02-16 13:46:07 +0100

    header color

--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -49,11 +49,10 @@ get_template_part('templates/tribe-events-attendee-list');
                     <?php the_content(); ?>
                 </div>
                 <div class="rehorik-tribe-events-single-hint">
-                    Stand 25.01.2022: Alle geplanten Termine finden unter Vorbehalt statt.
-                    <br>Unter den derzeit geltenden Corona-Auflagen können nur Kurse im Kaffeebereich durchgeführt werden.
+                    Stand 15.02.2022: Alle geplanten Termine finden unter Vorbehalt statt.
                     <br>Für alle unsere Veranstaltungen gelten die aktuellen Corona-Regelungen. Bitte denkt daran Eure Nachweise mitzunehmen, wir freuen uns auf Euch!
                     <br>2G+ Regel - geimpft oder genesen plus negativen Schnelltestnachweis. Für Geboosterte entfällt der Schnelltest.
-                    <br>Bitte beachtet, dass die Anmeldung verbindlich ist und nicht <b>verschoben, storniert oder umgetauscht</b> werden kann!
+                    <br>Bitte beachtet, dass die Anmeldung verbindlich ist und <b>nicht verschoben, storniert oder umgetauscht</b> werden kann!
                 </div>
             </div>
             <div>
