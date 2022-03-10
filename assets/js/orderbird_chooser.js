58c14fb2fdbf682974f3e5b958660f158a7af8b6
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2022-03-10 14:54:38 +0100

    orderbird

--- assets/js/orderbird_chooser.js
+++ assets/js/orderbird_chooser.js
@@ -1,25 +1,23 @@
 (() => {
-    const ids = ['cafe-190-grad', 'rehorik-rosterei-kaffehaus'];
-    const dropdown = document.getElementById('orderbird-chooser-dropdown');
-    const widgetContainer = document.getElementById('resmio-' + ids[0]);
     const head = document.getElementsByTagName('head')[0];
+    const widgetContainer = document.getElementById('resmio');
+    const radios = document.getElementsByName('orderbird-radio');
+    let activeId;
+
+    radios.forEach((radio) => {
+        const id = radio.value;
+        if (radio.checked) activeId = id;
+        radio.onchange = () => updateWidget(id);
+    })
+
     const updateWidget = (id) => {
         widgetContainer.setAttribute('id', 'resmio-' + id);
         widgetContainer.innerHTML = '';
-        if (this.widget) {
-            this.widget.remove();
-        }
+        if (this.widget) this.widget.remove();
         this.widget = document.createElement("script");
         this.widget.src = "//static.resmio.com/static/de/widget.js#id=" + id;
         head.appendChild(this.widget);
     }
 
-    updateWidget(ids[0]);
-    dropdown.onchange = (e) => {
-        const id = e.target.value;
-        if (!ids.includes(id)) {
-            throw 'Wrong location id.' + id + ' does not exist.'
-        }
-        updateWidget(id);
-    }
+    if (activeId) updateWidget(activeId);
 })();
--- templates/orderbird-chooser.php
+++ templates/orderbird-chooser.php
@@ -1,11 +1,13 @@
 <?php ?>
 <div id="orderbird-chooser">
-    <label>
-        <select id="orderbird-chooser-dropdown">
-            <option value="cafe-190-grad">Café 190°</option>
-            <option value="rehorik-rosterei-kaffehaus">Rösterei & Kaffeehaus</option>
-        </select>
+    <label for="cafe-190-grad">
+        Café 190°
+        <input type="radio" id="cafe-190-grad" value="cafe-190-grad" name="orderbird-radio" />
     </label>
-    <div id="resmio-cafe-190-grad"></div>
-    <h2>Reservierungen</h2>
+    <label for="rehorik-rosterei-kaffehaus">
+        Rösterei & Kaffeehaus
+        <input type="radio" id="rehorik-rosterei-kaffehaus" value="rehorik-rosterei-kaffehaus" name="orderbird-radio" />
+    </label>
+    <div id="resmio"></div>
+    <h2>Reservieren</h2>
 </div>
