a10744219687cc78ccd86975577b2ccfb56305f5
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-03-15 14:36:25 +0100

    style

--- templates/orderbird-chooser.php
+++ templates/orderbird-chooser.php
@@ -1,13 +1,15 @@
 <?php ?>
 <div id="orderbird-chooser">
+    <div class="orderbird-widget" id="resmio"></div>
     <label for="cafe-190-grad">
-        <h2>Café 190°</h2>
         <input type="radio" id="cafe-190-grad" value="cafe-190-grad" name="orderbird-radio" />
     </label>
     <label for="rehorik-rosterei-kaffehaus">
-        <h2>Rösterei & Kaffeehaus</h2>
         <input type="radio" id="rehorik-rosterei-kaffehaus" value="rehorik-rosterei-kaffehaus" name="orderbird-radio" />
     </label>
-    <div class="orderbird-widget" id="resmio"></div>
+    <div class="orderbird-location-names">
+        <h2>Café 190°</h2>
+        <h2>Rösterei & Kaffeehaus</h2>
+    </div>
     <h2>Reservieren</h2>
 </div>
