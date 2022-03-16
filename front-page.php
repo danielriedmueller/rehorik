470f032a19c7dd40a19d81c7f963eee51e96157c
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-03-16 09:25:17 +0100

    orderbird chooser

--- front-page.php
+++ front-page.php
@@ -78,9 +78,6 @@ get_header();
 </div>
 <div class="container hint-margin-top">
     <ul class="rehorik-products products">
-        <li class="product-category product">
-            <?php get_template_part('templates/orderbird-chooser'); ?>
-        </li>
         <?php
         $product_categories = getShopFrontPageCategories();
         foreach ($product_categories as $category) {
