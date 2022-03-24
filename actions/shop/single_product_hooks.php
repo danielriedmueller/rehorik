699b30065c82b262d6352e9eb9cf5275adaad6d8
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-03-24 14:36:33 +0100

    product detail page

--- actions/shop/single_product_hooks.php
+++ actions/shop/single_product_hooks.php
@@ -6,16 +6,15 @@ function remove_functions() {
     remove_action( 'woocommerce_after_single_product_summary', 'et_divi_output_product_wrapper_end', 0  );
 }
 
-
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 
+add_action( 'woocommerce_single_product_summary', 'rehorik_bio_sigil', 5);
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 10);
 add_action( 'woocommerce_single_product_summary', 'cup_of_coffee', 15);
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 20);
 
-add_action( 'woocommerce_after_single_product_summary', 'rehorik_bio_sigil', 5);
 add_action( 'woocommerce_after_single_product_summary', 'rehorik_product_video', 5);
 //add_action( 'woocommerce_after_single_product_summary', 'rehorik_single_product_attributes', 5);
 //add_action( 'woocommerce_after_single_product_summary', 'rehorik_hugo_head', 5);
@@ -46,43 +45,5 @@ function rehorik_hugo_head() {
 
 function cup_of_coffee() {
     global $product;
-    if (!isItCategory($product, COFFEE_CATEGORY_SLUG)) return;
-
-    $woocommerceGzProduct = wc_gzd_get_product($product);
-
-    $oneCupOfCoffeeInGrams = ONE_CUP_OF_COFFEE_IN_GRAMS;
-    $unitName = $woocommerceGzProduct->get_unit();
-    $unitBase = $woocommerceGzProduct->get_unit_base();
-    $unitAmount = $unitName === "kg"
-        ? 1000
-        : ($unitName === "g" ? 1 : null);
-
-    if ($unitAmount) {
-        $multiplier = $oneCupOfCoffeeInGrams / $unitAmount;
-
-        $unitPrice = empty($woocommerceGzProduct->get_unit_price())
-            ? $woocommerceGzProduct->get_variation_unit_prices()
-            : $woocommerceGzProduct->get_unit_price();
-
-        $formattedPriceRange = "";
-
-        if (is_numeric($unitPrice)) {
-            $priceForOneCup = $unitPrice * $multiplier * $unitBase;
-            $formattedPriceRange = wc_price($priceForOneCup);
-        }
-
-        if (is_array($unitPrice) && array_key_exists('price', $unitPrice)) {
-            $min = min(array_filter($unitPrice['price']));
-            $max = max(array_filter($unitPrice['price']));
-
-            $priceForOneCupMin =  number_format($min * $multiplier * $unitBase, 2);
-            $priceForOneCupMax = number_format($max * $multiplier * $unitBase, 2);
-
-            $formattedPriceRange = wc_format_price_range($priceForOneCupMin, $priceForOneCupMax);
-        }
-
-        if (!empty($formattedPriceRange)) {
-            echo "<div class='rehorik-cup-of-coffee-price-outer'>Eine Tasse Kaffee kostet nur <span class='rehorik-cup-of-coffee-price'>${formattedPriceRange}</span> (${oneCupOfCoffeeInGrams} g)</div>";
-        }
-    }
+    get_template_part('templates/cup-of-coffee', null, ['product' => $product]);
 }
