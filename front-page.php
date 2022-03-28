8a42cbfc64b3a40256f11f68a2ff107093b7de83
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-03-28 14:49:04 +0200

    tradition page

--- front-page.php
+++ front-page.php
@@ -1,46 +1,44 @@
-<?php get_header(); ?>
-<ul id="slider">
-    <?php get_template_part('templates/frontpage-header-item', null, [
-        'id' => 4,
+<?php
+get_header();
+get_template_part('templates/header/slider', null, ['items' => [
+    [
         'claim' => 'EL RUBI - MEHR ALS EIN EDELSTEIN',
+        'img'=> 'Produkt-Illustration-El-Rubi-header',
         'buttons' => [
             '/produkt/el-rubi' => 'Out now: Limited Edition Nr. 4 '
         ]
-    ]) ?>
-    <?php get_template_part('templates/frontpage-header-item', null, [
-        'id' => 1,
+    ],
+    [
         'claim' => 'Kaffeerösterei seit 1928',
+        'img'=> 'startseite-header-kaffee',
         'buttons' => [
             get_term_link(get_term_by('slug', COFFEE_CATEGORY_SLUG, 'product_cat'),
-                    'product_cat') => 'Zum Kaffee',
+                'product_cat') => 'Zum Kaffee',
             get_page_link(ROESTEREI_PAGE_ID) => 'Unser Kugelröster'
         ]
-    ]) ?>
-    <?php get_template_part('templates/frontpage-header-item', null, [
-        'id' => 2,
+    ],
+    [
         'claim' => 'TRADITION, QUALITÄT & LEIDENSCHAFT',
+        'img'=> 'startseite-header-roesterei',
         'buttons' => [
             get_term_link(get_term_by('slug', COFFEE_FILTERKAFFEE_CATEGORY_SLUG, 'product_cat'),
                 'product_cat') => 'Hier gibts Filterkaffee',
             get_term_link(get_term_by('slug', COFFEE_ESPRESSO_CATEGORY_SLUG, 'product_cat'),
                 'product_cat') => 'Und hier Espresso'
         ]
-    ]) ?>
-    <?php get_template_part('templates/frontpage-header-item', null, [
-        'id' => 3,
+    ],
+    [
         'claim' => 'WIR KÖNNEN AUCH WEIN',
+        'img'=> 'startseite-header-wein',
         'buttons' => [
             get_term_link(get_term_by('slug', WINE_SPIRITS_CO_CATEGORY_SLUG, 'product_cat'),
                 'product_cat') => 'Zur Weinauswahl',
             get_term_link(get_term_by('slug', WINE_SPIRITS_CO_CATEGORY_SLUG, 'product_cat'),
                 'product_cat') => 'Und Spirituosen'
         ]
-    ]) ?>
-</ul>
-<div id="tns-controls-container">
-    <button></button>
-    <button></button>
-</div>
+    ]
+]]);
+?>
 <a id="rehorik-logo" href="<?php echo get_home_url(); ?>"></a>
 <div class="page-title-outer">
     <div class='page-title'><h1>Shop</h1></div>
---
+++ templates/header/slider.php
@@ -0,0 +1,37 @@
+<?php
+$imgPathPrefix = 'url(../img/slider/';
+$imgLargePathSuffix = '-1920x600px.jpg';
+$imgMediumPathSuffix = '-1080x600px.jpg';
+$imgSmallPathSuffix = '-375x485px.jpg';
+$imgPathSuffix = ')';
+?>
+<ul id="slider">
+    <?php foreach($args['items'] as $item): ?>
+        <li>
+            <div class="slider-image"
+                 style='
+                 --image-small:<?=$imgPathPrefix.$item['img'].$imgSmallPathSuffix.$imgPathSuffix ?>;
+                 --image-medium:<?=$imgPathPrefix.$item['img'].$imgMediumPathSuffix.$imgPathSuffix ?>;
+                 --image-large:<?=$imgPathPrefix.$item['img'].$imgLargePathSuffix.$imgPathSuffix ?>;
+                 '
+            ></div>
+            <div class="slider-claim">
+                <?php if (isset($item['text'])): ?>
+                    <div class="auto-width slider-text"><div><?= $item['text'] ?></div></div>
+                <?php endif; ?>
+                <div class="slider-title"><h1><?= $item['claim'] ?></h1></div>
+                <div class="auto-width button-container">
+                    <div>
+                        <?php foreach ($item['buttons'] as $link => $text): ?>
+                            <a class="button" href="<?= $link ?>"><?= $text ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        </li>
+    <?php endforeach; ?>
+</ul>
+<div id="tns-controls-container">
+    <button></button>
+    <button></button>
+</div>
