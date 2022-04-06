dc3a91e4f3e1b2153695b82e92252838fa2bfaf1
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-04-06 12:15:12 +0200

    events frontpage

--- actions/shop/frontpage_categories.php
+++ actions/shop/frontpage_categories.php
@@ -2,23 +2,28 @@
 /**
  * Add coming next events to frontpage category pane
  */
-add_action('woocommerce_before_subcategory', function(WP_Term $category) {
+add_action('woocommerce_after_subcategory', function(WP_Term $category) {
     if ($category->slug !== TICKET_CATEGORY_SLUG){
         return;
     }
 
     $events = tribe_get_events([
-        'posts_per_page' => 5,
+        'posts_per_page' => 4,
         'start_date'     => 'now',
     ]);
 
-    $html = '<div class="rehorik-upcoming-events"><table>';
+    $html = '<div class="rehorik-upcoming-events">';
     foreach ($events as $event) {
         /** @var WP_Post $event */
-        $html .= sprintf('<tr><td>%s</td><td>%s</td></tr>', $event->post_title, $event->event_date);
+        $link = tribe_get_event_link($event->ID);
+        $html .= sprintf(
+            '<a href="%s"><span>%s</span><span>%s</span></a>',
+            $link,
+            date('d. M', strtotime($event->event_date)),
+            $event->post_title
+        );
     }
-
-    $html .= '</table></div>';
+    $html .= '</div>';
 
     echo $html;
 }, 10, 1);
