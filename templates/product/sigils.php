4910203a7918d9658b27b5266ac77b80ae7e2964
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-04-07 14:17:38 +0200

    biodynamic sigil

--- templates/product/sigils.php
+++ templates/product/sigils.php
@@ -1,13 +1,13 @@
 <?php $product = $args['product']; ?>
 <div class="sigil-container">
     <?php if (hasBioSigil($product)): ?>
-        <div class="<?=getBioSigilClass($product)?>"><?=getBioSigilControlcode($product)?></div>
-        <div class="<?=getBioSigilClass($product)?>-de"></div>
+        <div title="Bio" class="<?=getBioSigilClass($product)?>"><?=getBioSigilControlcode($product)?></div>
+        <div title="Bio" class="<?=getBioSigilClass($product)?>-de"></div>
     <?php endif; ?>
     <?php if (hasVeganSigil($product)): ?>
-        <div class="<?=getVeganSigilClass($product)?>"></div>
+        <div title="Vegan" class="<?=getVeganSigilClass($product)?>"></div>
     <?php endif; ?>
     <?php if (hasBiodynamicSigil($product)): ?>
-        <div class="<?= getBiodynamicSigilClass($product) ?>"></div>
+        <div title="Biodynamisch" class="<?= getBiodynamicSigilClass($product) ?>"></div>
     <?php endif; ?>
 </div>
