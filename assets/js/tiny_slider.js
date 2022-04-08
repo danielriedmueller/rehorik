a50a4007f229dadf61b49290ee8f4fa545993694
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-04-08 10:57:50 +0200

    slider update

--- assets/js/tiny_slider.js
+++ assets/js/tiny_slider.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    tns({
-        container: '#slider',
-        slideBy: 'page',
-        autoplay: false,
-        controlsPosition: 'bottom',
-        nav: false,
-        autoWidth: true,
-        controlsContainer: "#tns-controls-container"
-    });
+    if (document.getElementById('slider')) {
+        tns({
+            container: '#slider',
+            slideBy: 'page',
+            autoplay: false,
+            controlsPosition: 'bottom',
+            nav: false,
+            autoWidth: true,
+            controlsContainer: "#tns-controls-container"
+        });
+    }
 });
