badfa79969a0f829812ae19a10c900db6eef4166
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-04-13 13:18:44 +0200

    remove title

---
+++ templates/hint.php
@@ -0,0 +1,19 @@
+<?php ?>
+
+<div class="rehorik-hint">
+    <input type="checkbox">
+    <div>Aktuelle Informationen</div>
+    <div>
+        <div>
+            Endlich ist es soweit: die neuen Veranstaltungstermine sind online. Feiert, genießt & lernt mit uns.
+            <br>
+            Hier geht's zu den aktuellen <a href="/produkt-kategorie/veranstaltungen/">Events</a>.
+        </div>
+        <hr>
+        <div>
+            Aufgrund von Lieferproblemen beim Rohkaffe kommt es bei einigen Kaffeesorten zu einer
+            Versandverzögerung.
+            Wir bitten um Verständnis.
+        </div>
+    </div>
+</div>
