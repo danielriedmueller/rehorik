e6506c861be2fffc2009d6c7589dc2b4c2868728
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-04 12:43:52 +0200

    product search

--- templates/cart-total.php
+++ templates/cart-total.php
@@ -2,13 +2,5 @@
 if (!class_exists( 'woocommerce') || !WC()->cart) {
     return;
 }
-
-$items_number = WC()->cart->get_cart_contents_count();
-$items_number = $items_number > 0 ? "<div class='rehorik-cart-info-number'>${items_number}</div>" : "";
-$url = function_exists( 'wc_get_cart_url' ) ? wc_get_cart_url() : WC()->cart->get_cart_url();
-$url = esc_url($url);
-
 ?>
-<a href="<?= $url ?>" class="rehorik-cart-info">
-    <span></span><?= $items_number ?>
-</a>
+<a href="<?= wc_get_cart_url() ?>" class="rehorik-cart-info"><div class='rehorik-cart-info-number'><?php echo WC()->cart->get_cart_contents_count() > 0 ? WC()->cart->get_cart_contents_count() : "" ?></div></a>
