376060337249d0da7eb9b0377551ee0f0fc23b4d
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-05 10:04:24 +0200

    neue roesterei page

--- page-neue-roesterei.php
+++ page-neue-roesterei.php
@@ -3,210 +3,117 @@ get_header();
 get_template_part('templates/header/slider', null, [
     'items' => [
         [
-            'claim' => 'Vom Ursprung inspiriert',
-            'img' => 'header-tradition',
-            'buttons' => [
-                '/produkt/karlsbader-mischung' => 'Unsere Karslbader Mischung',
-            ],
-        ],
-        [
-            'claim' => 'Bis heute weitergelebt',
-            'img' => 'startseite-header-roesterei',
-            'buttons' => [
-                '/produkt/hochlandmischung' => 'Regensburger Mischung',
-            ],
+            'claim' => 'REHORIK KAFFEE<br>INTERGALAKTISCH GUT',
+            'img' => 'header-neue-roesterei',
         ],
     ],
 ]);
 ?>
     <a id="rehorik-logo" href="<?php echo get_home_url(); ?>"></a>
     <div class="page-title-outer">
-        <div class='page-title'><h1><?=the_title()?></h1></div>
+        <div class='page-title'><h1><?= the_title() ?></h1></div>
     </div>
-    <div class="rehorik-page-introduction-outer">
-        <div class="container">
-            <div class="rehorik-page-introduction">
-                Seit vier Generationen lebt Rehorik Kaffee, Wein und Feinkost. Damit hat Hugo Rehorik vor über 90 Jahren
-                begonnen. Und wir
-                tun es immer noch. Deshalb wissen wir genau, wie es geht und worauf wir achten müssen: Wo kommen die
-                Produkte her? Wie
-                werden sie produziert? Und besonders wichtig: Überzeugt uns die Qualität? Das überprüfen wir regelmäßig.
-                Wir
-                besuchen Kaffeebauern und Produzenten weltweit. Wir schauen uns um, probieren aus und bringen mit, was
-                uns gefällt – seit
-                1928 als familiengeführtes Unternehmen.
+    <div id="page-neue-roesterei">
+        <div>
+            <div class="container">
+                <div>
+                    <div><img alt="Rehorik Neue Roesterei"
+                              src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_Baustelle_11.jpg' ?>">
+                    </div>
+                    <div>
+                        <p class="mb-s">Die letzten Monate waren wir fleißig.<br>
+                            Was dabei herauskam? Eine schicke neue Rösterei mit Kaffeebar und Restaurantbetrieb in der
+                            <b>Straubinger Straße 62 a</b>.<br>
+                            Kleines Zuckerl on top: Ab sofort könnt Ihr Filterwasser bei uns abfüllen. Kanister
+                            mitbringen, Wasser abzapfen, fertig. Und für alle Nachtschwärmer:innen, Parties, Dinners &
+                            Tastings kommen in der neuen Rösterei natürlich auch nicht zu kurz.</p>
+                        <div><b>ÖFFNUNGSZEITEN</b></div>
+                        <table class="mb-s">
+                            <tbody>
+                            <tr>
+                                <td>MO. - FR.</td>
+                                <td>07:30 - 18:00 Uhr</td>
+                            </tr>
+                            <tr>
+                                <td>SA.</td>
+                                <td>09:00 - 18:00 Uhr</td>
+                            </tr>
+                            <tr>
+                                <td>SO.</td>
+                                <td>Geschlossen</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <div><b>ADRESSE</b></div>
+                        <div>Straubinger Straße 62a / 93055 Regensburg</div>
+                        <div>Telefon 0941 / 788 353 20</div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div id="timeline-outer">
-            <div id="timeline">
+        <div>
+            <div class="container">
+                <h1>RÖSTEREI & KAFFEEHAUS</h1>
                 <div>
-                    <section class="year">
-                        <h3>1928</h3>
-                        <section>
-                            <ul>
-                                <li><span>Firmengründung einer Kaffeerösterei mit Ladengeschäft von Hugo Rehorik in Karlsbad (CZ)</span></li>
-                                <li><img alt="Tradition 1928" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-1928.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1945</h3>
-                        <section>
-                            <ul>
-                                <li><span>Familie Rehorik wird vertrieben und lässt sich in Regensburg nieder</span></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1948</h3>
-                        <section>
-                            <ul>
-                                <li><span>Hugo gründet die Firma ‚Hugo Rehorik & Co.‘</span></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1950</h3>
-                        <section>
-                            <ul>
-                                <li><span>2. Generation Heinz Rehorik Heinz kommt nach seiner Musikerkarriere in elterlichen Betrieb</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-1950.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1952</h3>
-                        <section>
-                            <ul>
-                                <li><img class="narrow" alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-500px-1952.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1953</h3>
-                        <section>
-                            <ul>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-1953.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1960</h3>
-                        <section>
-                            <ul>
-                                <li><span>Heinz eröffnet die Weinabteilung</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-1960.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1969</h3>
-                        <section>
-                            <ul>
-                                <li><span>Der Brixener Hof wird saniert, die Rösterei wird dort eingebaut</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-1969.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1972</h3>
-                        <section>
-                            <ul>
-                                <li><span>Heinz eröffnet die Filiale im Donaueinkaufszentrum (DEZ)</span></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1981</h3>
-                        <section>
-                            <ul>
-                                <li><span>3. Generation Joachim Rehorik</span></li>
-                                <li><img class="narrow" alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-500x800px-1981.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1986</h3>
-                        <section>
-                            <ul>
-                                <li><span>Joachim eröffnet einen neuen, größeren Laden im DEZ mit Käsetheke</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-1986.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>1990</h3>
-                        <section>
-                            <ul>
-                                <li><span>Joachim eröffnet am Brixener Hof die Käseabteilung</span></li>
-                                <li><img class="narrow" alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-500x800px-1990.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>2000</h3>
-                        <section>
-                            <ul>
-                                <li><span>Joachim übernimmt einen Weinladen in der Gesandtenstraße</span></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>2001</h3>
-                        <section>
-                            <ul>
-                                <li><span>Direkter Einkauf aus Guatemala</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-2001.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>2011</h3>
-                        <section>
-                            <ul>
-                                <li><span>4. Generation Heiko Rehorik</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-2011.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>2013</h3>
-                        <section>
-                            <ul>
-                                <li><span>Einkauf aus Honduras</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-2013.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>2014</h3>
-                        <section>
-                            <ul>
-                                <li><span>Heiko gründet das Café 190° Am Brixener Hof 6</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-2014.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>2021</h3>
-                        <section>
-                            <ul>
-                                <li><span>Gründung des neuen Delicatessen Laden Am Brixener Hof 11</span></li>
-                                <li><img alt="Tradition" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-2021.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
-                    <section class="year">
-                        <h3>2021</h3>
-                        <section>
-                            <ul>
-                                <li><span>Neuer Standort: Rösterei & Kaffeehaus in der Straubinger Straße 62 A</span></li>
-                                <li><img alt="Tradition 2021" src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/tradition/tradition-bildmaterial-800x500px-2021-2.jpg' ?>"></li>
-                            </ul>
-                        </section>
-                    </section>
+                    <div>
+                        <p class="mb-s">
+                            <b>Kugelröster oder Trommelröster: Wir können beides!</b>
+                            Worauf wir sehr stolz sind: In der neuen Rösterei arbeiten wir an einem wunderschönen Giesen
+                            Trommelröster. Mehr Technik, mehr industrielle Röstung? Absolutely not! Mit unserem
+                            Trommelröster bringen wir das Rösten auf ein neues Level. Mehr Technik bedeutet mehr Detail,
+                            so können wir Süße und Säure des Kaffees ganz gezielt steuern.
+                            Und was passiert mit unserem Kugelröster am Brixener Hof? Nichts! So schnell darf das
+                            Kultstück nicht in Rente, denn wir bleiben unserem Handwerk treu. Wer am Kugelröster steht,
+                            muss die Bohnen verstehen und das richtige Feeling für den Kaffee haben – das ist Arbeit mit
+                            allen Sinnen. Und so leben wir das, was vor 90 Jahren schon bei uns gelebt wurde. Damals hat
+                            der Kugelröster den Rösterblues erfunden und das ist nach wie vor unser Lieblingssong!
+                            Und weil jede Bohne ihr eigenes Röstprofil hat, nutzen wir nun beide Techniken. Durch eine
+                            verglaste Wand für alle sichtbar – das ist Showrösten vom Allerfeinsten.
+                        </p>
+                        <img class="mb-s" alt="Rehorik Neue Roesterei"
+                             src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_Roesterei-kaffeehaus-1.jpg' ?>">
+                        <p class="mb-s">
+                            Und es geht noch mehr: Probiert Euch an der Kaffeebar durch unser Kaffeesortiment oder kommt
+                            zum Mittagessen auf eine Bowl vorbei. Euer Weinvorrat ist geplündert? In unserem
+                            Verkaufsbereich findet Ihr ausgewählte Weine, Spirits und Delikatessen. Und falls Ihr auf
+                            der Suche nach der perfekten Espressomaschine oder dem richtigen Kaffeezubehör seid – wir
+                            bringen einiges an Kaffeeequipment und viel Erfahrung mit, um Euch vor Ort zu beraten.
+                        </p>
+                        <img class="mb-s" alt="Rehorik Neue Roesterei"
+                             src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_Baustelle_9.jpg' ?>">
+                        <p>
+                            <b>» KOKUMI «</b> kommt aus dem Japanischen und bedeutet so viel wie <b>» Mundfülle «</b> –
+                            ein intensives, ausgewogenes Geschmackserlebnis. Und genau das bekommt Ihr von unserem
+                            Küchenchef Tim. Er spielt mit Aromen, Texturen und Temperaturen und hat den gesamten Prozess
+                            selbst in der Hand: vom ursprünglichen Produkt bis zum fertigen Gericht. Dabei
+                            experimentiert er mit verschiedenen Verarbeitungsmethoden wie Fermentation, Pökeln und
+                            Räuchern. Ein internationales Crossover mit Zutaten, die wir überwiegend aus der Region
+                            beziehen. Tradition neu interpretiert – so wie Ihr das von Rehorik kennt.<br>
+                            <b>Vom Frühstück und dem täglichen Mittagsangebot bis zu exklusiven Dinner Abenden.</b>
+                        </p>
+                    </div>
+                    <div>
+                        <img class="mb-s" alt="Rehorik Neue Roesterei"
+                             src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_Roesterei-kaffeehaus-3.jpg' ?>">
+                        <img class="mb-s" alt="Rehorik Neue Roesterei"
+                             src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_Roesterei-kaffeehaus-2.jpg' ?>">
+                        <img alt="Rehorik Neue Roesterei"
+                             src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_Roesterei-kaffeehaus-4.jpg' ?>">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="container">
+                <div>
+                    <img alt="Rehorik Neue Roesterei"
+                         src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/speisekarte-neue-roesterei-deckblatt.jpg' ?>">
+                    <img alt="Rehorik Neue Roesterei"
+                         src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_rosterei-kaffeehaus-10.jpg' ?>">
+                    <img alt="Rehorik Neue Roesterei"
+                         src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_rosterei-kaffeehaus-13.jpg' ?>">
+                    <img alt="Rehorik Neue Roesterei"
+                         src="<?= get_stylesheet_directory_uri() . '/assets/img/pages/roesterei/Rehorik_rosterei-kaffeehaus-11.jpg' ?>">
                 </div>
             </div>
         </div>
