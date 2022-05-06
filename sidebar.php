791a5d125ac7a0369dd04bbbd5df0bc5f2195b4f
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-06 10:47:45 +0200

    old sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,11 @@
 <?php if (is_active_sidebar('productfilter')) : ?>
     <div id="sidebar" class="rehorik-sidebar">
-        <?php dynamic_sidebar('productfilter'); ?>
+        <?php the_widget('WC_Widget_Product_Categories', [
+            'show_children_only' => true,
+            'count' => true,
+            'hide_empty' => false,
+            'hierarchical' => true,
+            'max_depth' => 3
+        ]); ?>
     </div>
 <?php endif; ?>
