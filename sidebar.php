3ea4410c4508d4da83872b9afb6fd69ed33ff906
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-09 13:31:21 +0200

    show empty

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
         <?php the_widget('WC_Widget_Product_Categories', [
             'show_children_only' => true,
             'count' => true,
-            'hide_empty' => true,
+            'hide_empty' => false,
             'hierarchical' => true,
             'max_depth' => 3
         ]); ?>
