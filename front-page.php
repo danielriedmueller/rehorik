e72e0d7b40130c90e15760df73107708b4adc007
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-13 10:28:44 +0200

    header bild

--- front-page.php
+++ front-page.php
@@ -2,6 +2,13 @@
 get_header();
 get_template_part('templates/header/slider', null, ['items' => [
     [
+        'claim' => 'Vatertag - nicht Katertag!',
+        'img'=> 'father2',
+        'buttons' => [
+            '/produkt-kategorie/onlineshop/spirits/brand/' => '15% Rabatt auf unsere guten Obstbrände'
+        ]
+    ],
+    [
         'claim' => 'EL RUBI - MEHR ALS EIN EDELSTEIN',
         'img'=> 'Produkt-Illustration-El-Rubi-header',
         'buttons' => [
