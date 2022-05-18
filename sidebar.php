a82ab333fd2148f93b4f7471083aeb6c6eb78f99
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-18 16:41:52 +0200

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,9 @@
-<?php if (is_active_sidebar('productfilter')) : ?>
-    <div id="sidebar" class="rehorik-sidebar">
-        <?php the_widget('WC_Widget_Product_Categories', [
-            'show_children_only' => true,
-            'count' => true,
-            'hide_empty' => false,
-            'hierarchical' => true,
-            'max_depth' => 3
-        ]); ?>
-    </div>
-<?php endif; ?>
+<div id="sidebar" class="rehorik-sidebar">
+    <?php the_widget('WC_Widget_Product_Categories', [
+        'show_children_only' => true,
+        'count' => true,
+        'hide_empty' => false,
+        'hierarchical' => true,
+        'max_depth' => 3
+    ]); ?>
+</div>
