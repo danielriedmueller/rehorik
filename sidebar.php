2ed4dbb38ff8c9366fca86e3e940184d94efcba8
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-19 14:52:59 +0200

    product count

--- sidebar.php
+++ sidebar.php
@@ -11,6 +11,8 @@
             'hierarchical' => true,
             'max_depth' => 3,
     ]); ?>
-    <h3 class="filter-title">Filter</h3>
-    <?= do_shortcode('[wpf-filters id=1]') ?>
+    <?php if (wc_get_loop_prop( 'total' ) > 0): ?>
+        <h3 class="filter-title">Filter</h3>
+        <?= do_shortcode('[wpf-filters id=1]') ?>
+    <?php endif; ?>
 </div>
