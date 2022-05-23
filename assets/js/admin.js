9225c7ebb06783214b907e5e546cf1dd97094fa4
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-23 14:58:24 +0200

    update price script

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const status = document.getElementById('status');
-    const button = document.getElementById('create-sigil-attributes-button');
-    button.onclick = () => {
-        status.innerHTML = 'Pending...';
-        jQuery.post(ajaxurl, {
-            'action': 'create_sigil_attributes',
-        }, function(response) {
-            status.innerHTML = response;
-        });
+    const updateSkuButton = document.getElementById('update-sku-button');
+    const updateCoffeePrice = document.getElementById('update-coffee-price-button');
+
+    if (updateSkuButton) {
+        updateSkuButton.onclick = () => {
+            status.innerHTML = 'Pending...';
+            jQuery.post(ajaxurl, {
+                'action': 'update_sku',
+            }, function(response) {
+                status.innerHTML = response;
+            });
+        }
+    }
+
+    if (updateCoffeePrice) {
+        updateCoffeePrice.onclick = () => {
+            status.innerHTML = 'Pending...';
+            jQuery.post(ajaxurl, {
+                'action': 'update_coffee_price',
+            }, function(response) {
+                status.innerHTML = response;
+            });
+        }
     }
 });
