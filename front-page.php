b753c52b8367298afc4c59b3d5a8e5135aa02e76
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-25 14:07:28 +0200

    header

--- front-page.php
+++ front-page.php
@@ -2,6 +2,14 @@
 get_header();
 get_template_part('templates/header/slider', null, ['items' => [
     [
+        'claim' => 'TRADITION, QUALITÄT & LEIDENSCHAFT',
+        'img'=> 'startseite-header-roesterei',
+        'buttons' => [
+            '/tradition' => 'Tradition',
+            '/standorte' => 'Standorte'
+        ]
+    ],
+    [
         'claim' => 'EL RUBI - MEHR ALS EIN EDELSTEIN',
         'img'=> 'Produkt-Illustration-El-Rubi-header',
         'buttons' => [
@@ -13,18 +21,7 @@ get_template_part('templates/header/slider', null, ['items' => [
         'img'=> 'startseite-header-kaffee',
         'buttons' => [
             get_term_link(get_term_by('slug', COFFEE_CATEGORY_SLUG, 'product_cat'),
-                'product_cat') => 'Zum Kaffee',
-            get_page_link(ROESTEREI_PAGE_ID) => 'Unser Kugelröster'
-        ]
-    ],
-    [
-        'claim' => 'TRADITION, QUALITÄT & LEIDENSCHAFT',
-        'img'=> 'startseite-header-roesterei',
-        'buttons' => [
-            get_term_link(get_term_by('slug', COFFEE_FILTERKAFFEE_CATEGORY_SLUG, 'product_cat'),
-                'product_cat') => 'Hier gibts Filterkaffee',
-            get_term_link(get_term_by('slug', COFFEE_ESPRESSO_CATEGORY_SLUG, 'product_cat'),
-                'product_cat') => 'Und hier Espresso'
+                'product_cat') => 'Zum Kaffee'
         ]
     ],
     [
