8185651584841038fd9497d25799ed216778d3ae
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-05-27 15:48:08 +0200

    start page claim

--- front-page.php
+++ front-page.php
@@ -47,6 +47,17 @@ get_template_part('templates/header/slider', null, ['items' => [
 ]]);
 ?>
 <a id="rehorik-logo" href="<?php echo get_home_url(); ?>"></a>
+<div class="page-title-outer">
+    <div class='page-title'><h1>Kaffe, Wein und Spirits aus Regensburg</h1></div>
+</div>
+<div class="rehorik-page-introduction-outer">
+    <div class="container">
+        <div class="rehorik-page-introduction">
+            Kaffee aus eigener Traditions-Rösterei seit vier Generationen vom Hause Rehorik. Wir führen auch eine hervorragend sortierte Wein- und Spirituosenauswahl.
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nulla
+        </div>
+    </div>
+</div>
 <?php get_template_part('templates/hint') ?>
 <div class="container hint-margin-top">
     <ul class="rehorik-products products">
