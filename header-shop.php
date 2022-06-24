a0be963d48dbd390934966323f29f049049f772d
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-06-24 11:35:17 +0200

    prevent multiple h1 on product page

--- header-shop.php
+++ header-shop.php
@@ -1,6 +1,8 @@
 <?php
-    get_template_part('templates/header/head');
+get_template_part('templates/header/head');
+if (!is_product()) {
     get_template_part('templates/page-title');
+}
 ?>
 <div class="container">
     <div id="main-content">
