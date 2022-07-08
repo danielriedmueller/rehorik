20abefa0f875c90bcb35b46afc6f4908c9415ad2
danielriedmueller <daniel.riedmuller@gmail.com>
2022-07-08 10:35:01 +0200

    lieferprobleme info text

--- templates/category/category-kaffee.php
+++ templates/category/category-kaffee.php
@@ -6,7 +6,7 @@ get_template_part('templates/page-title');
 ?>
     <div class="container">
         <div class="woocommerce-message" role="alert">
-            Aufgrund von Lieferproblemen beim Rohkaffee sind einige Kaffeesorten vorübergehend nicht lieferbar. Wir bitten Dich dafür um Verständnis. Hoffentlich können wir bald wieder rösten, was das Zeug hält!
+            Sorry, unsere Rohkaffeelieferung hat Verspätung, deswegen sind einige Sorten im Moment nicht lieferbar. Hoffentlich können wir bald wieder rösten, was das Zeug hält!
         </div>
         <div id="main-content">
 <?php
