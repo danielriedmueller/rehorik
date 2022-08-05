b24c7a5ffc5f903244a31e6846b686e9eef9d799
danielriedmueller <daniel.riedmuller@gmail.com>
2022-08-05 14:08:16 +0200

    tshirt bug fix

--- templates/product-preperation-recommendation.php
+++ templates/product-preperation-recommendation.php
@@ -8,14 +8,4 @@ $value = $product->get_meta('reh_preperation_recommendation');
 $seperator = "|";
 $textPieces = explode($seperator, $value);
 ?>
-<div class="rehorik-recommendation">
-    <?php if (!str_contains($value, $seperator)): ?>
-        <?= $value ?>
-    <?php else: ?>
-        <?php if (!empty($textPieces[0]) || !empty($textPieces[1])): ?>
-            <div><h5>Zubereitungsempfehlung</h5></div>
-            <?php if (!empty($textPieces[0])): ?><div><strong>Typ:</strong> <span><?= $textPieces[0] ?></span></div><?php endif; ?>
-            <?php if (!empty($textPieces[1])): ?><div><strong>Rezept:</strong> <span><?= $textPieces[1] ?></span></div><?php endif; ?>
-        <?php endif; ?>
-    <?php endif; ?>
-</div>
+<div class="rehorik-recommendation"><?php if (!str_contains($value, $seperator)): ?><?= $value ?><?php else: ?><?php if (!empty($textPieces[0]) || !empty($textPieces[1])): ?><div><h5>Zubereitungsempfehlung</h5></div><?php if (!empty($textPieces[0])): ?><div><strong>Typ:</strong> <span><?= $textPieces[0] ?></span></div><?php endif; ?><?php if (!empty($textPieces[1])): ?><div><strong>Rezept:</strong> <span><?= $textPieces[1] ?></span></div><?php endif; ?><?php endif; ?><?php endif; ?></div>
