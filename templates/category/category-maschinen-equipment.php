0bb749eead3e33b1d1c86c211a9b60bd58bd6977
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-08-08 10:30:40 +0200

    mail bugfix

--- templates/category/category-maschinen-equipment.php
+++ templates/category/category-maschinen-equipment.php
@@ -5,7 +5,7 @@ $slider = [
     'claim' => 'Werdet zum Home Barista und holt Euch Eure Lieblingsmaschine!',
     'img' => 'header-maschinen',
     'buttons' => [
-        'mailto:<?= BARISTASTORE_EMAIL ?>?subject=Beratungstermin%20Maschinen%20und%20Equipment' => 'Beratungstermin vereinbaren'
+        'mailto:' . BARISTASTORE_EMAIL . '?subject=Beratungstermin%20Maschinen%20und%20Equipment' => 'Beratungstermin vereinbaren'
     ]
 ];
 get_template_part('templates/header/head', null, ['slider' => [$slider]]);
