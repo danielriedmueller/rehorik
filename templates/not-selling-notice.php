b37f671872f4a32b036aaa7374ba7dc1c2668da1
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-08-09 16:27:16 +0200

    maschinen

--- templates/not-selling-notice.php
+++ templates/not-selling-notice.php
@@ -1,5 +1,5 @@
-<div>
+<div class="not-selling-notice">
     <p>Um Dir eine optimale Beratung geben zu können, verkaufen wir dir die Maschinen nicht online. Gerne führen wir Dich in die Welt der Siebträgermaschinen persönlich ein.</p>
     <p>Wir bitten Dich, einen Termin unter <a href="mailto:<?= BARISTASTORE_EMAIL ?>?subject=Maschinenberatung&body=Hallo%20Rehorik-Team,%0D%0A%0D%0AHIER%20STEHT%20DEINE%20NACHRICHT"><?= BARISTASTORE_EMAIL ?></a> zu vereinbaren.</p>
-    <p><strong>Wo:</strong> Ausstellungsraum Rehorik Rösterei & Kaffeehaus | <a target="_blank" href="https://goo.gl/maps/7syuH8WaSkARVtDf6">Straubinger Straße 62a</a></p>
 </div>
+<p><strong>Wo:</strong> Ausstellungsraum Rehorik Rösterei & Kaffeehaus | <a target="_blank" href="https://goo.gl/maps/7syuH8WaSkARVtDf6">Straubinger Straße 62a</a></p>
