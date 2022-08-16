c731aad1633c6b1eb94ec088bdbb8f951383321c
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-08-16 10:18:07 +0200

    product attributes bugfix

--- filter/product_view.php
+++ filter/product_view.php
@@ -2,7 +2,7 @@
 /**
  * Prevent comma seperated implode of values
  */
-add_filter( 'woocommerce_attribute', function( $formatted_values, $attribute, $values ) {
+add_filter('woocommerce_attribute', function ($formatted_values, $attribute, $values) {
     if (sizeof($values) > 1) {
         $formatted_values = implode("</li><li>", $values);
 
@@ -10,7 +10,7 @@ add_filter( 'woocommerce_attribute', function( $formatted_values, $attribute, $v
     }
 
     return "<span>" . $values[0] . "</span>";
-}, 10, 3 );
+}, 10, 3);
 
 /**
  * Add categories to attributes
@@ -18,15 +18,15 @@ add_filter( 'woocommerce_attribute', function( $formatted_values, $attribute, $v
  */
 add_filter('woocommerce_display_product_attributes', function ($productAttributes, $product) {
     $removeAttributesList = [
-        ATTRIBUTE_SLUG_PREFIX.GUETESIEGEL_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.BIOSIGIL_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.FILLING_QUANTITY_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.WEIGHT_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.WINERY_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.GOES_WITH_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.QUALITY_NAME_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.SIZE_ATTRIBUTE_SLUG,
-        WEIGHT_SLUG
+        ATTRIBUTE_SLUG_PREFIX . GUETESIEGEL_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . BIOSIGIL_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . FILLING_QUANTITY_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . WEIGHT_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . WINERY_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . GOES_WITH_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . QUALITY_NAME_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . SIZE_ATTRIBUTE_SLUG,
+        WEIGHT_SLUG,
     ];
 
     foreach ($removeAttributesList as $attribute) {
@@ -36,11 +36,11 @@ add_filter('woocommerce_display_product_attributes', function ($productAttribute
     /**
      * If Bio, add "BIO" beforehand Sorte value
      */
-    if ($productAttributes[ATTRIBUTE_SLUG_PREFIX.VARIETIES_ATTRIBUTE_SLUG]
+    if ($productAttributes[ATTRIBUTE_SLUG_PREFIX . VARIETIES_ATTRIBUTE_SLUG]
         && !empty(generateBioSigilControlcode($product->get_attribute(BIOSIGIL_ATTRIBUTE_SLUG)))
     ) {
-        $productAttributes[ATTRIBUTE_SLUG_PREFIX.VARIETIES_ATTRIBUTE_SLUG]["value"] = addBioBeforhandSorte(
-            $productAttributes[ATTRIBUTE_SLUG_PREFIX.VARIETIES_ATTRIBUTE_SLUG]["value"]
+        $productAttributes[ATTRIBUTE_SLUG_PREFIX . VARIETIES_ATTRIBUTE_SLUG]["value"] = addBioBeforhandSorte(
+            $productAttributes[ATTRIBUTE_SLUG_PREFIX . VARIETIES_ATTRIBUTE_SLUG]["value"]
         );
     }
 
@@ -48,24 +48,24 @@ add_filter('woocommerce_display_product_attributes', function ($productAttribute
      * Add Fuellmenge if single product
      */
     if (!$product->is_type('variable')) {
-        $value = wc_gzd_get_product( $product )->get_unit_product_html();
+        $value = wc_gzd_get_product($product)->get_unit_product_html();
         if (!empty($value)) {
-            $productAttributes[WEIGHT_SLUG] = array(
-                'label' => wc_gzd_get_product( $product )->get_unit() === "l" ? "Füllmenge" : "Gewicht",
-                'value' => wc_gzd_get_product( $product )->get_unit_product_html()
-            );
+            $productAttributes[WEIGHT_SLUG] = [
+                'label' => wc_gzd_get_product($product)->get_unit() === "l" ? "Füllmenge" : "Gewicht",
+                'value' => wc_gzd_get_product($product)->get_unit_product_html(),
+            ];
         }
     }
 
-    if (isset($productAttributes[ATTRIBUTE_SLUG_PREFIX.STRENGTH_ATTRIBUTE_SLUG])) {
-        $productAttributes[ATTRIBUTE_SLUG_PREFIX.STRENGTH_ATTRIBUTE_SLUG]['value'] = getStrengthFlavourHtml(
-            $productAttributes[ATTRIBUTE_SLUG_PREFIX.STRENGTH_ATTRIBUTE_SLUG]['value'],
+    if (isset($productAttributes[ATTRIBUTE_SLUG_PREFIX . STRENGTH_ATTRIBUTE_SLUG])) {
+        $productAttributes[ATTRIBUTE_SLUG_PREFIX . STRENGTH_ATTRIBUTE_SLUG]['value'] = getStrengthFlavourHtml(
+            $productAttributes[ATTRIBUTE_SLUG_PREFIX . STRENGTH_ATTRIBUTE_SLUG]['value'],
             'strength');
     }
 
-    if (isset($productAttributes[ATTRIBUTE_SLUG_PREFIX.FLAVOUR_VARIETY_ATTRIBUTE_SLUG])) {
-        $productAttributes[ATTRIBUTE_SLUG_PREFIX.FLAVOUR_VARIETY_ATTRIBUTE_SLUG]['value'] = getStrengthFlavourHtml(
-            $productAttributes[ATTRIBUTE_SLUG_PREFIX.FLAVOUR_VARIETY_ATTRIBUTE_SLUG]['value'],
+    if (isset($productAttributes[ATTRIBUTE_SLUG_PREFIX . FLAVOUR_VARIETY_ATTRIBUTE_SLUG])) {
+        $productAttributes[ATTRIBUTE_SLUG_PREFIX . FLAVOUR_VARIETY_ATTRIBUTE_SLUG]['value'] = getStrengthFlavourHtml(
+            $productAttributes[ATTRIBUTE_SLUG_PREFIX . FLAVOUR_VARIETY_ATTRIBUTE_SLUG]['value'],
             'flavour'
         );
     }
@@ -73,21 +73,21 @@ add_filter('woocommerce_display_product_attributes', function ($productAttribute
     /**
      * If Bohnenkomposition (Mischung) is present, it becomes the label for Sorte
      */
-    if (isset($productAttributes[ATTRIBUTE_SLUG_PREFIX.BEAN_COMPOSITION_ATTRIBUTE_SLUG])) {
-        if ($productAttributes[ATTRIBUTE_SLUG_PREFIX.BEAN_COMPOSITION_ATTRIBUTE_SLUG]
-            && $productAttributes[ATTRIBUTE_SLUG_PREFIX.VARIETIES_ATTRIBUTE_SLUG]) {
-            $productAttributes[ATTRIBUTE_SLUG_PREFIX.VARIETIES_ATTRIBUTE_SLUG]['label']
-                = $productAttributes[ATTRIBUTE_SLUG_PREFIX.BEAN_COMPOSITION_ATTRIBUTE_SLUG]['value'];
+    if (isset($productAttributes[ATTRIBUTE_SLUG_PREFIX . BEAN_COMPOSITION_ATTRIBUTE_SLUG])) {
+        if ($productAttributes[ATTRIBUTE_SLUG_PREFIX . BEAN_COMPOSITION_ATTRIBUTE_SLUG]
+            && $productAttributes[ATTRIBUTE_SLUG_PREFIX . VARIETIES_ATTRIBUTE_SLUG]) {
+            $productAttributes[ATTRIBUTE_SLUG_PREFIX . VARIETIES_ATTRIBUTE_SLUG]['label']
+                = $productAttributes[ATTRIBUTE_SLUG_PREFIX . BEAN_COMPOSITION_ATTRIBUTE_SLUG]['value'];
         }
     }
 
-    if (isset($productAttributes[ATTRIBUTE_SLUG_PREFIX.REGION_ATTRIBUTE_SLUG])) {
-        if ($productAttributes[ATTRIBUTE_SLUG_PREFIX.REGION_ATTRIBUTE_SLUG]
-            && $productAttributes[ATTRIBUTE_SLUG_PREFIX.ORIGIN_COUNTRY_ATTRIBUTE_SLUG]) {
-            $productAttributes[ATTRIBUTE_SLUG_PREFIX.REGION_ATTRIBUTE_SLUG]['value'] =
-                trim(strip_tags($productAttributes[ATTRIBUTE_SLUG_PREFIX.ORIGIN_COUNTRY_ATTRIBUTE_SLUG]['value']))
-                . ", " . trim(strip_tags($productAttributes[ATTRIBUTE_SLUG_PREFIX.REGION_ATTRIBUTE_SLUG]['value']));
-            unset($productAttributes[ATTRIBUTE_SLUG_PREFIX.ORIGIN_COUNTRY_ATTRIBUTE_SLUG]);
+    if (isset($productAttributes[ATTRIBUTE_SLUG_PREFIX . REGION_ATTRIBUTE_SLUG])) {
+        if ($productAttributes[ATTRIBUTE_SLUG_PREFIX . REGION_ATTRIBUTE_SLUG]
+            && $productAttributes[ATTRIBUTE_SLUG_PREFIX . ORIGIN_COUNTRY_ATTRIBUTE_SLUG]) {
+            $productAttributes[ATTRIBUTE_SLUG_PREFIX . REGION_ATTRIBUTE_SLUG]['value'] =
+                trim(strip_tags($productAttributes[ATTRIBUTE_SLUG_PREFIX . ORIGIN_COUNTRY_ATTRIBUTE_SLUG]['value']))
+                . ", " . trim(strip_tags($productAttributes[ATTRIBUTE_SLUG_PREFIX . REGION_ATTRIBUTE_SLUG]['value']));
+            unset($productAttributes[ATTRIBUTE_SLUG_PREFIX . ORIGIN_COUNTRY_ATTRIBUTE_SLUG]);
         }
     }
 
@@ -107,18 +107,18 @@ add_filter('woocommerce_display_product_attributes', function ($productAttribute
      *  2. Technische Daten
      */
     $sortedAttributes = [
-        ATTRIBUTE_SLUG_PREFIX.VINTAGE_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.STRENGTH_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.FLAVOUR_VARIETY_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.GRAPE_VARIETY_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.WINERY_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.REGION_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.AUSBAU_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.FLAVOUR_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.ALCOHOL_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . VINTAGE_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . STRENGTH_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . FLAVOUR_VARIETY_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . GRAPE_VARIETY_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . WINERY_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . REGION_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . AUSBAU_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . FLAVOUR_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . ALCOHOL_ATTRIBUTE_SLUG,
         WEIGHT_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.MANUFACTURER_ATTRIBUTE_SLUG,
-        ATTRIBUTE_SLUG_PREFIX.TECHNICAL_DETAILS_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . MANUFACTURER_ATTRIBUTE_SLUG,
+        ATTRIBUTE_SLUG_PREFIX . TECHNICAL_DETAILS_ATTRIBUTE_SLUG,
     ];
     uksort($productAttributes, function ($a, $b) use ($sortedAttributes) {
         if (array_search($a, $sortedAttributes) > array_search($b, $sortedAttributes)) {
@@ -131,9 +131,15 @@ add_filter('woocommerce_display_product_attributes', function ($productAttribute
     return $productAttributes;
 }, 10, 2);
 
-function addBioBeforhandSorte($value, $seperator = ", ") {
-    $values = array_map(function($value) {
+function addBioBeforhandSorte($value, $seperator = ", ")
+{
+    if (str_contains($value, "<li>")) {
+        return str_replace("<li>", "<li>BIO ", $value);
+    }
+
+    $values = array_map(function ($value) {
         return "BIO " . $value;
     }, explode($seperator, strip_tags($value, "<a>")));
+
     return "<p>" . implode($seperator, $values) . "</p>";
 }
