f40befc1439f8e27bf43e018569f04ec6523878b
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-08-18 09:33:43 +0200

    slider buttons

--- templates/featured/slider.php
+++ templates/featured/slider.php
@@ -6,23 +6,25 @@ if (empty($productList)) {
     return;
 }
 ?>
-<div class="slider-outer container" id="rehorik-featured-products">
-<?php if (sizeof($productList) > 1) : ?>
-    <ul id="slider-body" class="slider">
-        <?php foreach ($productList as $productId): ?>
-            <?php if ($product = wc_get_product($productId)): ?>
-                <li><?php get_template_part('templates/featured/item', null, ['product' => $product]) ?></li>
+<div class="slider-outer" id="rehorik-featured-products">
+    <div class="container">
+        <?php if (sizeof($productList) > 1) : ?>
+            <ul id="slider-body" class="slider">
+                <?php foreach ($productList as $productId): ?>
+                    <?php if ($product = wc_get_product($productId)): ?>
+                        <li><?php get_template_part('templates/featured/item', null, ['product' => $product]) ?></li>
+                    <?php endif;?>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+        <?php if (sizeof($productList) === 1) : ?>
+            <?php if ($product = wc_get_product($productList[0])): ?>
+                <?php get_template_part('templates/featured/item', null, ['product' => $product]) ?>
             <?php endif;?>
-        <?php endforeach; ?>
-    </ul>
+        <?php endif; ?>
+    </div>
     <div id="slider-body-controls" class="slider-controls">
         <button></button>
         <button></button>
     </div>
-<?php endif; ?>
-<?php if (sizeof($productList) === 1) : ?>
-    <?php if ($product = wc_get_product($productList[0])): ?>
-        <?php get_template_part('templates/featured/item', null, ['product' => $product]) ?>
-    <?php endif;?>
-<?php endif; ?>
 </div>
