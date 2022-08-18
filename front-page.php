3e383b84bf3738f0af954090fbaf47f6ade4332d
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-08-18 09:43:41 +0200

    cornerstone

--- front-page.php
+++ front-page.php
@@ -26,6 +26,15 @@ Wir verbinden Menschen durch Genuss,
 wir leben Leidenschaft für hochwertige Produkte. Und das seit Generationen.',
 ]);
 ?>
+<div class="rehorik-page-introduction-outer marketing-banner pflanze">
+    <div class="container">
+        <div class="rehorik-page-introduction">
+            <h3>Direkt und fair gehandelt</h3>
+            <span>Wir wählen und kaufen unsere Rohkaffees alle direkt bei unseren Bauern in den Ursprungsländern aus Deutschland, Neuseeland und Island. Damit können wir garantieren, dass diese einen fairen Preis für Ihren Kaffee erhalten Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibu</span>
+            <a class="button" href="">Entdecke mehr</a>
+        </div>
+    </div>
+</div>
 <div class="container">
     <ul class="rehorik-products products">
         <li class="product-category product">
