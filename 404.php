21bfb9471bb1cc2012454a4ca49e41fe924ac93c
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-09-14 10:27:29 +0200

    404

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="container">
     <div class="marketing-banner-text">
-        <h2>Aktuell können durch einen technischen Fehler keine Veranstaltungen gebucht werden. Bitte entschuldigt das und versucht es einfach später nochmal.</h2>
+        <h2>Tut mir leid, aber hier ist nichts.</h2>
     </div>
 </div>
 <?php get_footer(); ?>
