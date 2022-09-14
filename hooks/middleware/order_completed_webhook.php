b7ce9c7553684d9096cdb899b5b26f2197a0e7a5
danielriedmueller <daniel.riedmuller@gmail.com>
2022-09-14 11:22:11 +0200

    middleware

--- hooks/middleware/order_completed_webhook.php
+++ hooks/middleware/order_completed_webhook.php
@@ -1,27 +1,28 @@
 <?php
-add_filter( 'woocommerce_valid_webhook_events', function( $events ) {
+add_filter('woocommerce_valid_webhook_events', function ($events) {
     $events[] = 'completed';
     return $events;
-} );
+});
 
-add_filter( 'woocommerce_webhook_topics', function( $topics ) {
-    $topics['order.completed'] = __( 'Order completed', 'woocommerce' );
+add_filter('woocommerce_webhook_topics', function ($topics) {
+    $topics['order.completed'] = 'Bestellung abgeschlossen';
+
     return $topics;
-} );
+});
 
-add_filter( 'woocommerce_webhook_topic_hooks', function( $topic_hooks ) {
-    $hooks = array( 'woocommerce_order_status_completed' );
+add_filter('woocommerce_webhook_topic_hooks', function ($topic_hooks) {
+    $hooks = array('woocommerce_order_status_completed');
     $statuses = array_filter(
-        array_keys( wc_get_order_statuses() ),
-        function( $status ) {
+        array_keys(wc_get_order_statuses()),
+        function ($status) {
             return 'wc-completed' !== $status;
         }
     );
 
-    foreach ( $statuses as $status )
-        $hooks[] = 'woocommerce_order_status_completed_to_' . substr( $status, 3 );
+    foreach ($statuses as $status)
+        $hooks[] = 'woocommerce_order_status_completed_to_' . substr($status, 3);
 
     $topic_hooks['order.completed'] = $hooks;
 
     return $topic_hooks;
-} );
+});
