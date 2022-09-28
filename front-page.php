3579ace2822c0a3c9616a8aa002560d8452065b0
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-09-28 16:16:54 +0200

    coffee day event

--- front-page.php
+++ front-page.php
@@ -9,14 +9,6 @@ get_template_part('templates/header/head', null, ['slider' => [
         ]
     ],
     [
-        'claim' => 'Wir feiern den Kaffee! Feiert mit!',
-        'primary' => true,
-        'img'=> 'header-tag-des-kaffees-2',
-        'buttons' => [
-            '/produkt-kategorie/onlineshop/kaffee/' => '10% Rabatt am Tag des Kaffees'
-        ]
-    ],
-    [
         'claim' => 'weil Kaffee Dich auf die richtige Bahn bringt',
         'img'=> 'header-neue-roesterei',
         'buttons' => [
@@ -43,7 +35,7 @@ get_template_part('templates/header/head', null, ['slider' => [
 ]]);
 get_template_part('templates/hint');
 get_template_part('templates/introduction', null, [
-    'text' => '<span>Am <a href="https://www.internationalcoffeeday.org/">internationalen Tag des Kaffees</a> feiern wir diejenigen, die hart arbeiten, um die drei Milliarden köstlichen Tassen Kaffee herzustellen, die wir jeden Tag auf der ganzen Welt genießen. Besucht am Samstag, den 01.10., unsere professionellen Baristas im Donaueinkaufszentrum in Regensburg. Zwischen _UHRZEIT_BEGINN_ und _UHRZEIT_ENDE_ zeigen Sie euch, was alles in dieser wertvollen Bohne steckt. Oder stöbert ein bisschen hier im Onlineshop und entdeckt neue Lieblingssorten. Dafür gibts am 01.10. 10% Rabatt auf alle Kaffees.</span>',
+    'text' => '<span>Am <a href="https://www.internationalcoffeeday.org/">internationalen Tag des Kaffees</a> feiern wir diejenigen, die hart arbeiten, um die drei Milliarden köstlichen Tassen Kaffee herzustellen, die wir jeden Tag auf der ganzen Welt genießen. Wir wollen zeigen, was in und hinter der Bohne steckt und warum sie so schmeckt, wie sie schmeckt. Begebt Euch mit uns auf einer kleinen Weltreise und verkostet mit unserm Kaffeeakademie-Team am 01.10.22 in unserer Filiale im Donaueinkaufszenturm die Vielfalt des Kaffees. Nicht in Regensburg? Dann stöbert doch einfach ein bisschen hier im Onlineshop und entdeckt neue Lieblingssorten. Dafür gibt’s am 01.10.22 10% Rabatt auf alle Kaffees.</span>',
 ]);
 get_template_part('templates/featured/slider');
 ?>
