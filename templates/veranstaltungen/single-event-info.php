89297c0299fd4586790bd5b8946679bc2f0e18e4
danielriedmueller <daniel.riedmuller@gmail.com>
2022-10-06 13:38:41 +0200

    single event info hint

--- templates/veranstaltungen/single-event-info.php
+++ templates/veranstaltungen/single-event-info.php
@@ -1,7 +1,6 @@
 <div class="rehorik-tribe-events-single-hint">
-    Bitte beachtet, dass die Anmeldung verbindlich ist und <b>nicht verschoben, storniert oder umgetauscht</b> werden kann!
-</div>
-<div class="rehorik-tribe-events-single-hint">
     Falls es bei Eurer Veranstaltung die Möglichkeit gibt einen Deliteller zu buchen, dann werdet ihr mit feinen Käse- und Wurstspezialitäten aus unserem Deliladen überrascht. Bei vegetarischen Delitellern ersetzen wir die Wurst natürlich durch eine weitere Käseauswahl.
     Bitte beachtet, dass unsere Küche am Abend der Veranstaltung geschlossen ist und wir leider keine spontanen Essens-Wünsche erfüllen können.
+    <br><br>
+    Bitte beachtet, dass die Anmeldung verbindlich ist und <b>nicht verschoben, storniert oder umgetauscht</b> werden kann!
 </div>
