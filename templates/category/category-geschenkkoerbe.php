d6afef41b7d0f80fd32f8f53b4434f7bd5b82bb5
danielriedmueller <daniel.riedmuller@gmail.com>
2022-10-13 10:14:22 +0200

    category geschenkkoerbe

--- templates/category/category-geschenkkoerbe.php
+++ templates/category/category-geschenkkoerbe.php
@@ -7,25 +7,15 @@ get_template_part('templates/header/head', null, [
             'claim' => 'Einfach für jeden Anlass',
             'img' => 'header-geschenkkoerbe',
             'buttons' => [
-                '/' => 'TODO',
+                '/' => 'Etwas für unterm Baum gesucht?<br>Unser Hiliger Bimbam Geschenkkorb',
             ],
         ],
     ],
 ]);
 get_template_part('templates/introduction', null, [
-    'text' => 'NOCH NICHT DAS PASSENDE DABEI??<br>Alle Pakete im Flyer sind Vorschläge aus unserem Sortiment. Natür-
-lich erstellen wir auch Geschenke ganz nach Euren Vorstellungen –
-einfach anrufen, eine Mail schreiben oder persönlich vorbeikommen.<br>Telefon 0941 / 586 52 76 ODER E-Mail brixener@rehorik.de<br>
-UM EUCH DEN BESTEN SERVICE ZU BIETEN,
-BEACHTET BITTE FOLGENDES:
-<ul>
-    <li>Individuell gesteckte Geschenkkörbe ab 80€ nur auf Vorbestellung und mit 2 Tagen Vorlauf</li>
-    <li>Aufträge, die per Post verschickt werden, benötigen von der Bearbeitung bis zur
-    Zustellung circa 5 Werktage.</li>
-    <li>Gesteckte Geschenkkörbe können nicht per Post verschickt werden. In diesem Fall
-    wird die Ware bruchsicher in einen Geschenkkarton verpackt.</li>
-    <li>Die angegebenen Preise sind freibleibend.</li>
-</ul>',
+    'text' => 'NOCH NICHT DAS PASSENDE DABEI?<br>Alle Pakete im Flyer sind Vorschläge aus unserem Sortiment.<br>Natürlich erstellen wir auch Geschenke ganz nach Euren Vorstellungen –
+einfach anrufen, eine Mail schreiben oder persönlich vorbeikommen.<br>Telefon 0941 / 586 52 76 oder E-Mail brixener@rehorik.de<br>
+',
 ]);
 get_template_part('templates/page-title');
 ?>
