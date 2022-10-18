272acdae6dbe07f0390c2c1bad17ad177b62030f
danielriedmueller <daniel.riedmuller@gmail.com>
2022-10-18 08:41:05 +0200

    frontpage categories

--- helper/category_helper.php
+++ helper/category_helper.php
@@ -130,7 +130,6 @@ function getShopFrontPageCategories()
     $frontPageCategories[] = $categories[array_search(WINE_CATEGORY_SLUG, $keys)];
     $frontPageCategories[] = $categories[array_search(SPIRITS_CATEGORY_SLUG, $keys)];
     $frontPageCategories[] = $categories[array_search(TICKET_CATEGORY_SLUG, $keys)];
-    $frontPageCategories[] = $categories[array_search(GIFTS_CATEGORY_SLUG, $keys)];
 
     return $frontPageCategories;
 }
