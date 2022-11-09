af822fa6847048d823bb4cafbf03a1c809b697f9
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-11-09 10:11:01 +0100

    new front page hint

--- templates/hint.php
+++ templates/hint.php
@@ -1,22 +1,46 @@
 <?php ?>
-<div class="rehorik-hint">
-    <input type="checkbox">
-    <h5>Aktuelle Informationen</h5>
-    <div>
+<div class="rehorik-hints">
+    <div class="rehorik-hint">
+        <input type="checkbox">
+        <h5>Speisekarten & Öffnungszeiten</h5>
         <div>
-            Am 25.9.2022 findet unsere jährliche Firmenfeier statt. Das Café 190° hat aufgrund dessen an diesem Tag geschlossen.
-        </div>
-        <hr>
-        <div>
-            Endlich ist es soweit: die neuen Veranstaltungstermine sind online. Feiert, genießt & lernt mit uns.
-            <br>
-            Hier geht's zu den aktuellen <a href="/produkt-kategorie/veranstaltungen/">Events</a>.
-        </div>
-        <hr>
-        <div>
-            Aufgrund von Lieferproblemen beim Rohkaffee kommt es bei einigen Kaffeesorten zu einer
-            Versandverzögerung.
-            Wir bitten um Verständnis.
+            <div>
+                Am 25.9.2022 findet unsere jährliche Firmenfeier statt. Das Café 190° hat aufgrund dessen an diesem Tag geschlossen.
+            </div>
+            <hr>
+            <div>
+                Endlich ist es soweit: die neuen Veranstaltungstermine sind online. Feiert, genießt & lernt mit uns.
+                <br>
+                Hier geht's zu den aktuellen <a href="/produkt-kategorie/veranstaltungen/">Events</a>.
+            </div>
+            <hr>
+            <div>
+                Aufgrund von Lieferproblemen beim Rohkaffee kommt es bei einigen Kaffeesorten zu einer
+                Versandverzögerung.
+                Wir bitten um Verständnis.
+            </div>
         </div>
     </div>
+    <div class="rehorik-hint">
+        <input type="checkbox">
+        <h5>Aktuelle Informationen</h5>
+        <div>
+            <div>
+                Am 25.9.2022 findet unsere jährliche Firmenfeier statt. Das Café 190° hat aufgrund dessen an diesem Tag geschlossen.
+            </div>
+            <hr>
+            <div>
+                Endlich ist es soweit: die neuen Veranstaltungstermine sind online. Feiert, genießt & lernt mit uns.
+                <br>
+                Hier geht's zu den aktuellen <a href="/produkt-kategorie/veranstaltungen/">Events</a>.
+            </div>
+            <hr>
+            <div>
+                Aufgrund von Lieferproblemen beim Rohkaffee kommt es bei einigen Kaffeesorten zu einer
+                Versandverzögerung.
+                Wir bitten um Verständnis.
+            </div>
+        </div>
+    </div>
+
 </div>
