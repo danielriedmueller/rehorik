c179c270eb77ee23ca5b731e5a762c56e4f3e71c
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-11-11 13:21:37 +0100

    thank you page

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -30,7 +30,7 @@ get_template_part('templates/header/slider', null, [
     <div class="container">
         <div class="rehorik-page-introduction">
             <span>
-            Du hast von uns eine E-Mail mit den Details deiner Bestellung erhalten.<br>Hast du noch Fragen oder sind Probleme aufgetreten?<br>Schreib uns einfach eine Mail an <a href="mailto:<?= CONTACT_MAIL ?>?subject=Kundenanfrage&body=Hallo%20Rehorik-Team,%0D%0A%0D%0AHIER%20STEHT%20DEINE%20NACHRICHT"><?= CONTACT_MAIL ?></a><br>oder ruf an <a href="tel:<?= CONTACT_PHONE ?>"><?= CONTACT_PHONE ?></a>
+            Du hast von uns eine E-Mail mit den Details deiner Bestellung erhalten.<br>Hast du noch Fragen oder sind Probleme aufgetreten?<br>Schreib uns einfach eine Mail an <a href="mailto:<?= CONTACT_MAIL ?>?subject=Kundenanfrage&body=Hallo%20Rehorik-Team,%0D%0A%0D%0AHIER%20STEHT%20DEINE%20NACHRICHT"><?= CONTACT_MAIL ?></a><br>oder ruf uns an <a href="tel:<?= CONTACT_PHONE ?>"><?= CONTACT_PHONE ?></a>
                 </span>
         </div>
     </div>
