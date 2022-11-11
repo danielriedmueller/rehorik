ee1a853157b9c467aa213db5d9480d857be04481
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-11-11 13:53:41 +0100

    thank you page

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
-<?php if (!is_checkout() && empty( is_wc_endpoint_url('order-received'))) : ?>
+<?php if (is_wc_endpoint_url('order-received')) : ?>
+    <?php
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post();
+            the_content();
+        }
+    }
+    ?>
+<?php else: ?>
     <?php get_template_part('templates/page-title'); ?>
     <div class="container">
         <?php
@@ -11,14 +20,5 @@
         }
         ?>
     </div>
-<?php else: ?>
-    <?php
-    if ( have_posts() ) {
-        while ( have_posts() ) {
-            the_post();
-            the_content();
-        }
-    }
-    ?>
 <?php endif; ?>
 <?php get_footer(); ?>
