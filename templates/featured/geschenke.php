412ad52a12c64df444098937bc71e02342b15dbe
Daniel Riedmüller <daniel.riedmuller@gmail.com>
2022-11-16 10:02:11 +0100

    typo

--- templates/featured/geschenke.php
+++ templates/featured/geschenke.php
@@ -36,7 +36,7 @@ $link = "/produkt-kategorie/onlineshop/geschenke-gutscheine/geschenke/";
     <div class="text">
         <h3><a href="<?= $link ?>">Geschenke</a></h3>
         <span class="claim">Etwas für unterm Baum gesucht?</span>
-        <span class="description">Hier findet Ihr die perfekten Geschenke für befreundete Feinschmecker:innen oder Schmankerl für verwandt Genießer:innen. Wir haben das Beste aus unseren Wein- und Delikatessenregalen geholt und schon mal ein paar Geschenk zusammengestellt.</span>
+        <span class="description">Hier findet Ihr die perfekten Geschenke für befreundete Feinschmecker:innen oder Schmankerl für verwandte Genießer:innen. Wir haben das Beste aus unseren Wein- und Delikatessenregalen geholt und schon mal ein paar Geschenk zusammengestellt.</span>
         <span class="learn-more"><a href="<?= $link ?>">erfahre mehr</a></span>
     </div>
 </div>
