1dad5b10c1dce939eea471f937479ac7d14c8413
danielriedmueller <daniel.riedmuller@gmail.com>
2022-11-24 16:12:49 +0100

    onlinecoupon text

--- templates/category/category-gutscheine.php
+++ templates/category/category-gutscheine.php
@@ -4,13 +4,13 @@ defined('ABSPATH') || exit;
 get_template_part('templates/header/head', null, [
     'slider' => [
         [
-            'claim' => 'Zum Verschenken oder Selbsteinlösen!',
+            'claim' => 'Genuss zum Verschenken oder Selbsteinlösen!',
             'img' => 'header-gutscheine',
         ],
     ],
 ]);
 get_template_part('templates/introduction', null, [
-    'text' => 'Unsere Onlinegutscheine sind einlösbar für alle Veranstaltungen und alle Produkte, die sich hier so finden lassen.<br>Noch ein Hinweis: Die Onlinegutscheine sind nur im Onlineshop einlösbar.<br>An unseren Standorten kann man mit den Onlinegutscheinen leider noch nicht shoppen gehen.',
+    'text' => 'Zum selbst Ausdrucken und direkt nach dem Kauf verwendbar. Für Veranstaltungen, Kaffee und alle Produkte, die sich hier so finden lassen.<br>Noch ein Hinweis: Die Onlinegutscheine sind nur im Onlineshop einlösbar. An unseren Standorten kann man mit den Onlinegutscheinen leider noch nicht shoppen gehen.',
 ]);
 get_template_part('templates/page-title');
 ?>
