1bdb5e5564f0a14f736207ab0dd81e2d4ae59408
danielriedmueller <daniel.riedmuller@gmail.com>
2022-11-25 11:30:39 +0100

    wording

--- templates/category/category-geschenke.php
+++ templates/category/category-geschenke.php
@@ -14,7 +14,7 @@ get_template_part('templates/header/head', null, [
 ]);
 get_template_part('templates/introduction', null, [
     'text' => '<span>Hier findet Ihr die perfekten Geschenke für befreundete Feinschmecker:innen oder Schmankerl für verwandte Genießer:innen. Wir haben das Beste aus unseren Wein- und Delikatessenregalen geholt und schon mal ein paar Geschenk zusammengestellt.
-</span><span>Mit unserem bruchsicheren Versand überleben Rehorik Weihnachtsgeschenke auch die wildeste Schlittenfahrt, direkt zu Eurer Familie, Euren Kolleg:innen oder Freund:innen nach Hause. Der Verpackungsaufwand ist höher, deswegen müssen wir hier leider 2 € aufschlagen. Das Weihnachtswichtel-Team dankt!</span>',
+</span><span>Mit unserem bruchsicheren Versand überleben Rehorik Weihnachtsgeschenke auch die wildeste Schlittenfahrt, direkt zu Eurer Familie, Euren Kolleg:innen oder Freund:innen nach Hause. Der Verpackungsaufwand ist höher, deswegen müssen wir hier 2 € aufschlagen. Das Weihnachtswichtel-Team dankt!</span>',
 ]);
 get_template_part('templates/page-title');
 ?>
