75226e7a4bdd424daec7bd59123f7db4420ff9e3
danielriedmueller <daniel.riedmuller@gmail.com>
2022-12-02 15:07:10 +0100

    event info text

--- templates/veranstaltungen/single-event-info.php
+++ templates/veranstaltungen/single-event-info.php
@@ -1,4 +1,4 @@
 <div class="rehorik-tribe-events-single-hint">
     Ihr wollt eine Veranstaltung verschenken, aber seid Euch beim Termin unsicher? Dann schaut gerne mal bei unseren <a href="/produkt-kategorie/onlineshop/geschenke-gutscheine/gutscheine/">Gutscheinen</a> vorbei.
-    Bitte beachtet, dass die Anmeldung verbindlich ist und <b>nicht verschoben, storniert oder umgetauscht</b> werden kann!
+    <br />Bitte beachtet, dass die Anmeldung verbindlich ist und <b>nicht verschoben, storniert oder umgetauscht</b> werden kann!
 </div>
