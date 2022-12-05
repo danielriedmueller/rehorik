9e634a7ef2cf4e374604398c7f1b05dbec11e4ec
danielriedmueller <daniel.riedmuller@gmail.com>
2022-12-05 15:42:02 +0100

    add to cart error message

--- actions/ajax/add_to_cart.php
+++ actions/ajax/add_to_cart.php
@@ -5,22 +5,42 @@ add_action('wp_ajax_nopriv_rehorik_ajax_add_to_cart', 'rehorik_add_to_cart');
 function rehorik_add_to_cart()
 {
     $product_id = absint($_POST['product_id']);
-    $quantity = empty($_POST['quantity']) ? 1 : wc_stock_amount(wp_unslash($_POST['quantity']));
+    $variation_id = absint($_POST['variation_id']);
     $product = wc_get_product($product_id);
-    $variation_id = 0;
     $variation = array();
+    $product_url = apply_filters('woocommerce_cart_redirect_after_error', get_permalink($product_id), $product_id);
+    $error_contact_msg = sprintf(
+        'Hoppla, hier gibts ein technisches Problem. Bitte schreibe uns eine Mail an <a href="%s">%s</a> und wir schauen uns die Sache an oder probiere es einfach später nochmal.',
+        IT_SUPPORT_EMAIL,
+        IT_SUPPORT_EMAIL
+    );
 
     if (!$product) {
-        wp_send_json(['error' => true]);
+        wc_add_notice($error_contact_msg, 'error', ['product_id' => $product_id]);
+        wp_send_json([
+            'error' => true,
+            'product_url' => $product_url
+        ]);
 
         return;
     }
 
     $type = $product->get_type();
 
-    if ('variation' === $product->get_type()) {
-        $variation_id = $product_id;
-        $product_id = $product->get_parent_id();
+    if (('variable' === $type && $variation_id === 0)
+        || 'variable' !== $type && $variation_id !== 0) {
+        wc_add_notice($error_contact_msg, 'error', ['product_id' => $product_id]);
+        wp_send_json([
+            'error' => true,
+            'product_url' => $product_url
+        ]);
+
+        return;
+    }
+
+    $quantity = empty($_POST['quantity']) ? 1 : wc_stock_amount(wp_unslash($_POST['quantity']));
+
+    if ('variable' === $type) {
         $variation = $product->get_variation_attributes();
     }
 
@@ -29,15 +49,18 @@ function rehorik_add_to_cart()
 
     if ($passed_validation && false !== WC()->cart->add_to_cart($product_id, $quantity, $variation_id, $variation) && 'publish' === $product_status) {
         do_action('woocommerce_ajax_added_to_cart', $product_id);
-        if ('yes' === get_option('ql_woocommerce_cart_redirect_after_add')) {
-            wc_add_to_cart_message(array($product_id => $quantity), true);
-        }
+
+        // TODO Remove or reuse
+        /**
+         * if ('yes' === get_option('ql_woocommerce_cart_redirect_after_add')) {
+         * wc_add_to_cart_message(array($product_id => $quantity), true);
+         * }
+         */
         WC_AJAX:: get_refreshed_fragments();
     } else {
-        // If there was an error adding to the cart, redirect to the product page to show any errors.
         wp_send_json([
             'error' => true,
-            'product_url' => apply_filters('woocommerce_cart_redirect_after_error', get_permalink($product_id), $product_id),
+            'product_url' => $product_url
         ]);
     }
 }
