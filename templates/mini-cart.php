c6d230d8fe4bb2d9002b5464c782a76b22ebf42f
danielriedmueller <daniel.riedmuller@gmail.com>
2022-12-15 15:31:57 +0100

    mini cart mobile style

--- templates/mini-cart.php
+++ templates/mini-cart.php
@@ -3,10 +3,8 @@
     <div class="mini-cart-content">
         <div id="mini-cart-close">Weiter einkaufen</div>
         <div>
-            <div class="mini-cart-content-featured">
-                <?php get_template_part('templates/featured/reorder') ?>
-            </div>
             <div class="widget_shopping_cart_content"><?php wc_get_template('cart/mini-cart'); ?></div>
+            <div class="mini-cart-content-featured"><?php get_template_part('templates/featured/reorder') ?></div>
         </div>
     </div>
 </div>
