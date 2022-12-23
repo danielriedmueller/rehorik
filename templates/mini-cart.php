c31859652ac2acefa719584d19c6425376952599
danielriedmueller <daniel.riedmuller@gmail.com>
2022-12-23 09:24:15 +0100

    wc get template fix

--- templates/mini-cart.php
+++ templates/mini-cart.php
@@ -3,7 +3,7 @@
     <div class="mini-cart-content">
         <div id="mini-cart-close">Weiter einkaufen</div>
         <div>
-            <div class="widget_shopping_cart_content"><?php wc_get_template('cart/mini-cart'); ?></div>
+            <div class="widget_shopping_cart_content"><?php wc_get_template('cart/mini-cart', [], '', '/' . get_template_directory()); ?></div>
             <div class="mini-cart-content-featured"><?php get_template_part('templates/featured/reorder') ?></div>
         </div>
     </div>
