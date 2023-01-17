d49f2cf65f7a5cdaa3686fd78826f7aaca3d1eae
danielriedmueller <daniel.riedmuller@gmail.com>
2023-01-17 10:38:12 +0100

    cronjob

--- includes/class-reh-product-feed.php
+++ includes/class-reh-product-feed.php
@@ -5,7 +5,23 @@ class Reh_Product_Feed
     // Fields from plugin Germanized
     const GERMANIZED_FIELDS = ['unit_amount', 'unit_regular_price', 'unit'];
 
+    const CRON_HOOK = 'reh_product_feed';
+
     protected static $_instance = null;
+
+    public function __construct()
+    {
+        /**
+         * $this->loader->add_action( 'admin_init', $plugin_admin, 'register_weekly_cron');
+         * $this->loader->add_action('rex_feed_weekly_update', $plugin_admin, 'activate_weekly_update');
+
+        $this->define_admin_hooks();
+         if( ! wp_next_scheduled( 'rex_feed_weekly_update' ) ) {
+        wp_schedule_event( time(), 'weekly', 'rex_feed_weekly_update' );
+         $error = wp_schedule_event(time(), 'daily', 'reh_feed_daily', true);
+        }
+         **/
+    }
 
     public static function instance()
     {
@@ -27,6 +43,11 @@ class Reh_Product_Feed
         ];
 
         $this->updateFeedForAtalanda($args);
+    }
+
+    public function deactivate_feed(): void
+    {
+        wp_clear_scheduled_hook(self::CRON_HOOK);
     }
 
     public function updateFeedForAtalanda(array $args): void
