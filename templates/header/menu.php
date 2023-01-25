988d1687461d3e01d9f9798699c2e89933e1bb57
danielriedmueller <daniel.riedmuller@gmail.com>
2023-01-25 17:20:06 +0100

    top bar

--- templates/header/menu.php
+++ templates/header/menu.php
@@ -1,14 +1,25 @@
 <div class="rehorik-header-outer">
     <header class="rehorik-header" data-scrollpos>
-        <?php
-        get_template_part('templates/header/hamburger');
-        wp_nav_menu(['theme_location' => 'main']);
-        get_template_part('templates/social-media-icons', null, ['withLogo' => true]);
-        get_template_part('templates/header/cart-total');
-        if (is_active_sidebar('productsearch')) {
-            dynamic_sidebar('productsearch');
-        }
-        ?>
+        <div id="top-bar">
+            <div>Mit ❤️ aus Regensburg</div>
+            <div>ShopVote ★★★★★</div>
+            <div>Versandkostenfrei ab 69€</div>
+            <div>Hilfe / Beratung</div>
+            <?php
+            wp_nav_menu(['theme_location' => 'top-bar']);
+            get_template_part('templates/header/cart-total');
+            if (is_active_sidebar('productsearch')) {
+                dynamic_sidebar('productsearch');
+            }
+            ?>
+        </div>
+        <div id="rehorik-menu">
+            <?php
+            get_template_part('templates/header/hamburger');
+            wp_nav_menu(['theme_location' => 'main']);
+            get_template_part('templates/social-media-icons', null, ['withLogo' => true]);
+            ?>
+        </div>
     </header>
     <?php get_template_part('templates/social-media-icons'); ?>
 </div>
