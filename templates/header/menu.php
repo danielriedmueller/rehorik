dd77f4e0c08f7f06ad6b768418e32c4729e66ef4
danielriedmueller <daniel.riedmuller@gmail.com>
2023-01-26 12:22:07 +0100

    topbar

--- templates/header/menu.php
+++ templates/header/menu.php
@@ -1,17 +1,19 @@
 <div class="rehorik-header-outer">
     <header class="rehorik-header" data-scrollpos>
         <div id="top-bar">
-            <?php
-            if (is_active_sidebar('productsearch')) {
-                dynamic_sidebar('productsearch');
-            }
-            ?>
-            <div>
+            <div class="top-bar-left">
+                <?php
+                if (is_active_sidebar('productsearch')) {
+                    dynamic_sidebar('productsearch');
+                }
+                ?>
+            </div>
+            <div class="top-bar-center">
                 <div>Hilfe / Beratung: <?= CONTACT_PHONE ?></div>
                 <div>Mit <span class="bean-icon"></span> aus Regensburg</div>
                 <div>Versandkostenfrei ab 69€</div>
             </div>
-            <div>
+            <div class="top-bar-right">
                 <?php
                 get_template_part('templates/header/cart-total');
                 wp_nav_menu(['theme_location' => 'top-bar']);
