6791630aeba63d5d97b0db10b631fc60484f0e5d
danielriedmueller <danielriedmuller@gmail.com>
2023-02-01 10:28:38 +0100

    kontakte page

--- page-kontakt.php
+++ page-kontakt.php
@@ -5,23 +5,19 @@ get_template_part('templates/header/head', null, [
             'claim' => 'Wir helfen dir weiter',
             'img' => 'header-standorte',
             'buttons' => [
-                    '/standorte' => 'Unsere Standorte'
+                '/standorte' => 'Unsere Standorte'
             ],
         ],
     ],
 ]);
+get_template_part('templates/introduction', null, [
+    'text' => 'Bei Rehorik arbeiten viele Menschen. Alle kompetent und shit. Wir helfen dir bei einfach allem. Wir kennen uns so gut mit Kaffee und Genuss aus, es ist der Wahnsinn!',
+]);
 ?>
-<div class="rehorik-page-introduction-outer">
-    <div class="container">
-        <div class="rehorik-page-introduction">
-            Bei Rehorik arbeiten viele Menschen. Alle kompetent und shit. Wir helfen dir bei einfach allem. Wir kennen uns so gut mit Kaffee und Genuss aus, es ist der Wahnsinn!
-        </div>
-    </div>
-</div>
 <div class="container">
     <?php
-    if ( have_posts() ) {
-        while ( have_posts() ) {
+    if (have_posts()) {
+        while (have_posts()) {
             the_post();
             the_content();
         }
