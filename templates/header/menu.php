11c18990f98ab66d4c72938c41397654f0cdb7b0
danielriedmueller <danielriedmuller@gmail.com>
2023-02-02 13:17:40 +0100

    top bar mobile

--- templates/header/menu.php
+++ templates/header/menu.php
@@ -2,32 +2,12 @@
     <header class="rehorik-header" data-scrollpos>
         <?php get_template_part('templates/header/hamburger'); ?>
         <div id="rehorik-menu">
-            <div id="top-bar">
-                <div class="left">
-                    <div class="help-icon">Hilfe & Kontakt: <?= CONTACT_PHONE ?></div>
-                </div>
-                <div class="center">
-
-                    <div class="bean-icon">Mit <span></span> aus Regensburg</div>
-                    <div class="shipping-icon">Versandkostenfrei ab <?= FREE_SHIPPING_AMOUNT ?> €</div>
-                </div>
-                <div class="right">
-                    <?php
-                    if (is_active_sidebar('productsearch')) {
-                        dynamic_sidebar('productsearch');
-                    }
-                    get_template_part('templates/header/cart-total');
-                    foreach (wp_get_nav_menu_items('top-bar') as $item) {
-                        echo sprintf('<a href="%s" class="%s">%s</a>', $item->url, implode(' ', $item->classes), $item->title);
-                    }
-                    ?>
-                </div>
-            </div>
             <?php
+            get_template_part('templates/header/social-media-icons', null, ['withLogo' => true]);
             wp_nav_menu(['theme_location' => 'main']);
-            get_template_part('templates/social-media-icons', null, ['withLogo' => true]);
+            get_template_part('templates/header/top-bar');
             ?>
         </div>
     </header>
-    <?php get_template_part('templates/social-media-icons'); ?>
+    <?php get_template_part('templates/header/social-media-icons'); ?>
 </div>
