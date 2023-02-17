1a894abe7fbd77415ce176f8b6e3f92191420aae
danielriedmueller <daniel.riedmuller@gmail.com>
2023-02-17 10:25:21 +0100

    seo texts

--- woocommerce/single-product/product-attributes.php
+++ woocommerce/single-product/product-attributes.php
@@ -27,7 +27,7 @@ if (!$product_attributes) {
     <table>
         <?php foreach ($product_attributes as $product_attribute_key => $product_attribute) : ?>
             <tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr($product_attribute_key); ?>">
-                <th class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post($product_attribute['label']); ?></th>
+                <td class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post($product_attribute['label']); ?></td>
                 <td class="woocommerce-product-attributes-item__value"><?php echo wp_kses_post($product_attribute['value']); ?></td>
             </tr>
         <?php endforeach; ?>
