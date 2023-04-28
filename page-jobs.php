ebe842b053bf0328639f9dc892fd41a4eee83ae5
danielriedmueller <daniel.riedmuller@gmail.com>
2023-04-28 10:51:24 +0200

    jobs page

--- page-jobs.php
+++ page-jobs.php
@@ -18,42 +18,48 @@ get_template_part('templates/header/head', null, [
             beim morgendlichen Meeting und der ein oder anderen kleinen Party!
         </div>
     </div>
-    <div class="container">
-        <div class="job-description">
-            <h3>Zur Unterstützung unseres Teams, insbesondere in der Produktion, suchen wir ab sofort <br>- Eine/n</h3>
-            <h2>Produktionshelfer/In-</h2>
+    <div class="job-description-outer">
+        <div class="container">
+            <div class="job-description">
+                <h5>Zur Unterstützung unseres Teams, insbesondere in der Produktion, suchen wir ab sofort</h5>
+                <h2>- Eine/n Produktionshelfer/In-</h2>
 
-            <div>
-                <strong>Deine Aufgaben:</strong>
-                <ul>
-                    <li>Vorplanung von Bestellungen</li>
-                    <li>Abwicklung der Aufträge</li>
-                    <li>Zusammenarbeit mit Rösterei und Lager</li>
-                </ul>
-            </div>
-            <div>
-                <strong>Das bringst du mit:</strong>
-                <ul>
-                    <li>Du hast Humor</li>
-                    <li>Du stehst Morgens auf um Kaffee zu trinken, nicht umgekehrt!</li>
-                    <li>Du hast einen Sinn für hochwertige Produkte</li>
-                    <li>Du bringst Teamfähigkeit, strukturierte und selbstständige Arbeitsweisen und ein hohes Maß an Eigeninitiative</li>
-                </ul>
-            </div>
-            <div>
-                <strong>Deine Vorteile:</strong>
-                <ul>
-                    <li>Flexible Arbeitszeiten</li>
-                    <li>Ein dynamisches Team mit viel persönlichen Entfaltungsmöglichkeiten</li>
-                    <li>Entspannte und offene Unternehmenskultur</li>
-                    <li>Zugang zu einem breitgefächerten Netzwerk aus Kaffee, Wein, Gastronomie und Event Branche</li>
-                    <li>Rabatte auf Rehorik Produkte</li>
-                    <li>Der beste Kaffee der Welt (Kaffee vor Ort für Mitarbeiter gratis)</li>
-                </ul>
-            </div>
-            <div>
-                Dein Kopfkino arbeitet und du siehst dich schon mit Espresso Tasse neue Aufträge koordinieren?<br>
-                Dann sende uns eine Email mit kurzem Lebenslauf und deiner Motivation für diese Stelle an: <a href="mailto:<?= JOBS_MAIL ?>"><?= JOBS_MAIL ?></a>
+                <div>
+                    <strong>Deine Aufgaben:</strong>
+                    <ul>
+                        <li>Vorplanung von Bestellungen</li>
+                        <li>Abwicklung der Aufträge</li>
+                        <li>Zusammenarbeit mit Rösterei und Lager</li>
+                    </ul>
+                </div>
+                <div>
+                    <strong>Das bringst du mit:</strong>
+                    <ul>
+                        <li>Du hast Humor</li>
+                        <li>Du stehst Morgens auf um Kaffee zu trinken, nicht umgekehrt!</li>
+                        <li>Du hast einen Sinn für hochwertige Produkte</li>
+                        <li>Du bringst Teamfähigkeit, strukturierte und selbstständige Arbeitsweisen und ein hohes Maß
+                            an Eigeninitiative
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <strong>Deine Vorteile:</strong>
+                    <ul>
+                        <li>Flexible Arbeitszeiten</li>
+                        <li>Ein dynamisches Team mit viel persönlichen Entfaltungsmöglichkeiten</li>
+                        <li>Entspannte und offene Unternehmenskultur</li>
+                        <li>Zugang zu einem breitgefächerten Netzwerk aus Kaffee, Wein, Gastronomie und Event Branche
+                        </li>
+                        <li>Rabatte auf Rehorik Produkte</li>
+                        <li>Der beste Kaffee der Welt (Kaffee vor Ort für Mitarbeiter gratis)</li>
+                    </ul>
+                </div>
+                <div>
+                    Dein Kopfkino arbeitet und du siehst dich schon mit Espresso Tasse neue Aufträge koordinieren?<br>
+                    Dann sende uns eine Email mit kurzem Lebenslauf und deiner Motivation für diese Stelle an: <a
+                            href="mailto:<?= JOBS_MAIL ?>"><?= JOBS_MAIL ?></a>
+                </div>
             </div>
         </div>
     </div>
