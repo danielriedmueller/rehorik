07fbf7aaf4204a2575d8fe2c4c793c13344e8388
danielriedmueller <daniel.riedmuller@gmail.com>
2023-05-08 15:21:03 +0200

    stripe style

--- hooks/shop/stripe_hooks.php
+++ hooks/shop/stripe_hooks.php
@@ -12,8 +12,3 @@ if (!class_exists('WC_Stripe_Payment_Request')) {
 remove_action('woocommerce_proceed_to_checkout', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_html'], 1);
 add_action('woocommerce_proceed_to_checkout', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_html'], 20);
 remove_action('woocommerce_proceed_to_checkout', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_separator_html'], 2);
-
-// Display stripe payment request button in checkout
-remove_action('woocommerce_checkout_before_customer_details', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_html'], 1);
-remove_action('woocommerce_checkout_before_customer_details', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_separator_html'], 2);
-add_action('woocommerce_review_order_before_submit', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_html'], 1);
