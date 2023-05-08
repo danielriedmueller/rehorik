298a3325fd3fd019a9aa46026ae09d7f9eeabb96
danielriedmueller <daniel.riedmuller@gmail.com>
2023-05-08 14:52:28 +0200

    stripe checkout button

--- hooks/shop/stripe_hooks.php
+++ hooks/shop/stripe_hooks.php
@@ -17,8 +17,3 @@ remove_action('woocommerce_proceed_to_checkout', [WC_Stripe_Payment_Request::ins
 remove_action('woocommerce_checkout_before_customer_details', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_html'], 1);
 remove_action('woocommerce_checkout_before_customer_details', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_separator_html'], 2);
 add_action('woocommerce_review_order_before_submit', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_html'], 1);
-
-// Display stripe payment request button in single product
-remove_action('woocommerce_after_add_to_cart_quantity', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_html'], 1);
-remove_action('woocommerce_after_add_to_cart_quantity', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_separator_html'], 2);
-add_action('woocommerce_single_product_summary', [WC_Stripe_Payment_Request::instance(), 'display_payment_request_button_html'], 30);
