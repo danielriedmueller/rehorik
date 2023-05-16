39f5fb23a4bbeb1f4ede89b5b30def82815eee35
danielriedmueller <daniel.riedmuller@gmail.com>
2023-05-16 09:47:17 +0200

    payment methods

--- templates/footer.php
+++ templates/footer.php
@@ -16,8 +16,18 @@ get_template_part('templates/rehorik-locations');
             <div>
                 <div>
                     <h3>Zahlungsarten</h3>
-                    <p>Vorkasse</p>
-                    <p>Paypal, Kreditkarte</p>
+                    <div class="payment-methods">
+                        <div class="paypal"></div>
+                        <div class="apple-pay"></div>
+                        <div class="google-pay"></div>
+                        <div class="visa"></div>
+                        <div class="mastercard"></div>
+                        <div class="american-express"></div>
+                        <div class="giropay"></div>
+                        <div class="sofort"></div>
+                        <div class="vorkasse"></div>
+                        <div class="sepa"></div>
+                    </div>
                 </div>
             </div>
             <div>
