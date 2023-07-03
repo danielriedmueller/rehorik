f2eee3684821ae3e63c1526efe6805b7565824df
danielriedmueller <daniel.riedmuller@gmail.com>
2023-07-03 14:27:49 +0200

    create coffees script

--- actions/admin/rehorik/create_coffees.php
+++ actions/admin/rehorik/create_coffees.php
@@ -1,9 +1,47 @@
 <?php
 add_action('wp_ajax_create_coffees', function () {
     try {
-        //$skuParts = ['FSUM', 'EROS', 'E190'];
-        $skuParts = ['EROS'];
-        $skuPartsWeight = ['250', '500', '1000'];
+        $skuParts = [
+            'EDIA' => ['250', '500', '1000'],
+            'EKEN' => ['250', '500', '1000'],
+            'ESPA' => ['250', '500', '1000'],
+            'ENUO' => ['250', '500', '1000'],
+            'EPRS' => ['250', '500', '1000'],
+            'E190' => ['250', '500', '1000'],
+            'EROS' => ['250', '500', '1000'],
+            'EARD' => ['250', '500', '1000'],
+            'EPRI' => ['250', '500', '1000'],
+            'ECAF' => ['250', '500', '1000'],
+            'EVIC' => ['250', '500', '1000'],
+            'EENT' => ['250', '500', '1000'],
+            'EBER' => ['250', '500', '1000'],
+            'EAMA' => ['250', '500', '1000'],
+            'EWHA' => ['250', '500', '1000'],
+            'FFES' => ['250', '500', '1000'],
+            'CSCH' => ['250', '500', '1000'],
+            'FREG' => ['250', '500', '1000'],
+            'FSPE' => ['250', '500', '1000'],
+            'FWEI' => ['250', '500', '1000'],
+            'FPRE' => ['250', '500', '1000'],
+            'FKAR' => ['250', '500', '1000'],
+            'FWHA' => ['250', '500', '1000'],
+            'FOST' => ['250', '500', '1000'],
+            'FMOK' => ['250', '500', '1000'],
+            'FPAS' => ['250', '500', '1000'],
+            'FMON' => ['250', '500', '1000'],
+            'FMAL' => ['250', '500', '1000'],
+            'FHOR' => ['250', '500', '1000'],
+            'FENT' => ['250', '500', '1000'],
+            'FCAS' => ['250', '500', '1000'],
+            'FVIC' => ['250', '500', '1000'],
+            'FSUM' => ['250', '500', '1000'],
+            'FSPA' => ['250', '500', '1000'],
+            'FAMA' => ['250', '500', '1000'],
+            'FRUB' => ['250'],
+            'FJAV' => ['250'],
+            'CAMA' => ['250', '500', '1000'],
+        ];
+
         $skuPartsGrind = [
             '0' => 'ganze-bohne',
             '1' => 'aeropress',
@@ -16,17 +54,21 @@ add_action('wp_ajax_create_coffees', function () {
             '8' => 'filtermaschine'
         ];
 
-        foreach ($skuParts as $skuPart) {
+        $message = '';
+
+        foreach ($skuParts as $skuPart => $weights) {
             $products = wc_get_products(['sku' => $skuPart]);
             if (count($products) !== 1) {
-                throw new Exception('Product with SKU ' . $skuPart . ' not found.');
+                $message .= 'Product with SKU ' . $skuPart . ' not found. <br>';
+                continue;
             }
 
             /** @var WC_Product_Variable $product */
             $product = $products[0];
 
             if (!$product->is_type('variable')) {
-                throw new Exception('Product with SKU ' . $skuPart . ' is not variable.');
+                $message .= 'Product with SKU ' . $skuPart . ' is not variable. <br>';
+                continue;
             }
 
             $variationIds = $product->get_children();
@@ -40,28 +82,25 @@ add_action('wp_ajax_create_coffees', function () {
             // Then create new variations
             $newVariations = [];
 
-            foreach ($skuPartsWeight as $skuPartWeight) {
+            foreach ($weights as $weight) {
                 foreach ($skuPartsGrind as $skuPartGrind => $slug) {
-                    $sku = $skuPartGrind.$skuPart.$skuPartWeight;
-                    $newPrice = 6.50;
+                    $sku = $skuPartGrind . $skuPart . $weight;
+                    $message .= $sku . ' created <br>';
 
                     $variation = new WC_Product_Variation();
                     $variation->set_parent_id($product->get_id());
                     $variation->set_sku($sku);
                     $variation->set_attributes([
                         'pa_mahlgrad' => $slug,
-                        'pa_gewicht' => $skuPartWeight . 'g',
+                        'pa_gewicht' => $weight . 'g',
                     ]);
-                    $unit = $skuPartWeight / 1000;
+                    $unit = $weight / 1000;
                     $variation->set_weight($unit);
-
                     $gzd_product = wc_gzd_get_gzd_product($variation);
-                    $gzd_product->get_wc_product()->set_regular_price($newPrice);
-                    $gzd_product->set_unit_price_regular($newPrice);
-                    $gzd_product->set_unit_price($newPrice);
+                    $gzd_product->set_unit_product($unit);
 
-                    $gzd_product->get_wc_product()->save();
                     $gzd_product->save();
+                    $variation->save();
                     $newVariations[] = $variation;
                 }
             }
@@ -69,7 +108,7 @@ add_action('wp_ajax_create_coffees', function () {
             $product->set_children($newVariations);
         }
 
-        wp_send_json_success();
+        wp_send_json_success(['message' => $message]);
     } catch (Exception $e) {
         wp_send_json_error($e->getMessage());
     }
