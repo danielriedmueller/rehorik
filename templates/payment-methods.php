2a453b6c6d158c32379b56bd4afa90dc7f182fda
danielriedmueller <daniel.riedmuller@gmail.com>
2023-07-19 09:59:57 +0200

    payment methods deactivated

--- templates/payment-methods.php
+++ templates/payment-methods.php
@@ -1,13 +1,10 @@
 <?php ?>
 <div class="payment-methods">
-    <div class="paypal"></div>
     <div class="apple-pay"></div>
     <div class="google-pay"></div>
+    <div class="paypal"></div>
     <div class="visa"></div>
     <div class="mastercard"></div>
     <div class="american-express"></div>
-    <div class="giropay"></div>
-    <div class="sofort"></div>
     <div class="vorkasse"></div>
-    <div class="sepa"></div>
 </div>
