5511c68623c16b42273ce93574c49410a6167dfa
danielriedmueller <daniel.riedmuller@gmail.com>
2023-07-21 11:48:55 +0200

    google pay

--- templates/payment-methods.php
+++ templates/payment-methods.php
@@ -1,6 +1,7 @@
 <?php ?>
 <div class="payment-methods">
     <div class="apple-pay"></div>
+    <div class="google-pay"></div>
     <div class="paypal"></div>
     <div class="visa"></div>
     <div class="mastercard"></div>
