bbdcdf54d00e7352e1f6d9c36236d90df4b826a1
Daniel Riedmueller <daniel.riedmueller@hallowelt.com>
2025-02-22 13:42:52 +0100

    ausstellungsraum auf gesandenstrasse geändert

--- templates/product/not-selling-notice.php
+++ templates/product/not-selling-notice.php
@@ -4,7 +4,7 @@ $saleInfo = $product->get_attribute(SALE_INFO_ATTRIBUTE_SLUG);
 ?>
 <div class="not-selling-notice">
     <p>Um Dir eine optimale Beratung geben zu können, verkaufen wir dir die Maschinen nicht online. Gerne führen wir Dich in die Welt der Siebträgermaschinen persönlich ein.</p>
-    <p><strong>Wo:</strong> Ausstellungsraum Rehorik Rösterei & Kaffeehaus | <a target="_blank" href="https://goo.gl/maps/7syuH8WaSkARVtDf6">Straubinger Straße 62a</a></p>
+    <p><strong>Wo:</strong> Ausstellungsraum Rehorik Gesandtenstraße | <a target="_blank" href="https://maps.app.goo.gl/B2o9wcSW9ozUxvPW9">Gesandtenstraße 16</a></p>
     <p class="mb-l">Wir bitten Dich, einen Termin auszuwählen oder schreib uns unter <a href="mailto:<?= BARISTASTORE_EMAIL ?>?subject=Maschinenberatung&body=Hallo%20Rehorik-Team,%0D%0A%0D%0AHIER%20STEHT%20DEINE%20NACHRICHT"><?= BARISTASTORE_EMAIL ?></a>.</p>
     <?php
     get_template_part('templates/product/make-machine-appointment');
